b5920e4e666523aac4116c03625db314ea726fe0
Angelo Dell'Aera <buffer@olografix.org>
2011-08-01 15:14:38 +0200

    Minor changes in shellcode detection/emulation

--- src/Debugger/Debugger.py
+++ src/Debugger/Debugger.py
@@ -17,20 +17,14 @@
 # MA  02111-1307  USA
 
 import os
-import threading
 import logging
 import PyV8
 
-class Debugger(PyV8.JSDebugger, threading.Thread):
+class Debugger(PyV8.JSDebugger):
     log = logging.getLogger("dbg")
 
     def __init__(self, debug = False):
         PyV8.JSDebugger.__init__(self)
-        threading.Thread.__init__(self, name='dbg')
-        
-        self.terminated  = False
-        self.exitcode    = None
-        self.daemon      = True
         self.evalContext = PyV8.JSContext()
 
         if debug:
@@ -38,27 +32,25 @@ class Debugger(PyV8.JSDebugger, threading.Thread):
 
     def __enter__(self):
         script_filename = os.path.join(os.path.dirname(__file__), 'd8.js')
-
-        self.log.debug("loading d8.js from %s", script_filename)
-
         with self.context as ctxt:
             ctxt.eval(open(script_filename, 'r').read())
 
-        return PyV8.JSDebugger.__enter__(self)
+        self.setEnabled(True)
 
+        return PyV8.JSDebugger.__enter__(self)
+        
     def onMessage(self, msg):
         self.log.debug("Debug message: %s" % (msg, ))
-        
+       
         if msg['type'] == 'event' and msg['event'] == 'break':
-            self.stepNext()
-        
+           self.stepNext()
         return True
 
     def onDebugEvent(self, type, state, evt):
         json = evt.toJSONProtocol()
         self.log.debug("%s event: %s" % (type, json, ))
 
-    def onBreak(self, evt):
+    def onBreakEvent(self, evt):
         self.log.debug("Break event: %s" % (evt, ))
 
     def onException(self, evt):
@@ -69,7 +61,15 @@ class Debugger(PyV8.JSDebugger, threading.Thread):
 
     def onBeforeCompile(self, evt):
         self.log.debug("Before compile event: %s" % (evt, ))
+        return True
 
     def onAfterCompile(self, evt):
         self.log.debug("After compile event: %s" % (evt, ))
+        return True
+
+    def processDebugEvent(self, evt):
+        self.log.debug("Received debug event: %s", repr(event))
+
+    def onBreak(self):
+        self.log.debug("onBreak")
 
--- src/Debugger/Shellcode.py
+++ src/Debugger/Shellcode.py
@@ -16,32 +16,60 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
-import PyV8
+import PyV8 
+import string
 import struct
 import logging
 import pylibemu
 from Debugger import Debugger
 
+logging.basicConfig()
+
 class Shellcode:
     log = logging.getLogger("Shellcode")
+    emu = pylibemu.Emulator()
 
-    def __init__(self, ctxt, ast, script, debug = True):
-        self.script = script
-        self.ctxt   = ctxt
-        self.ast    = ast
-        self.emu    = pylibemu.Emulator()
+    def __init__(self, ctxt, ast, script, debug = False):
+        self.script  = script
+        self.ctxt    = ctxt
+        self.ast     = ast
+        self.offsets = set()
+
         if debug:
             self.log.setLevel(logging.DEBUG)
+            
+    def search_url(self, sc):
+        offset = sc.find('http')
+        
+        if offset > 0:
+            url = sc[offset:].split()[0]
+            self.log.debug('[Shellcode] URL Detected: %s' % (url, ))
+
+    def build_shellcode(self, s):
+        try:
+            shellcode = s.decode('utf-8')
+        except:
+            shellcode = s
+
+        sc = b''
+        try:
+            for c in shellcode:
+                sc += struct.pack('<H', ord(c))
+        except:
+            self.log.debug(traceback.print_exc())
+            return None
+
+        return sc
 
     def run(self):
         with Debugger() as dbg:
-            emu = pylibemu.Emulator()
             vars = self.ctxt.locals
+            #dbg.debugBreak()
             self.ctxt.eval(self.script)
 
             for name in self.ast.names:
-                s    = None
-                skip = False
+                s      = None
+                libemu = False
 
                 if name in vars.keys():
                     s = vars[name]
@@ -49,7 +77,6 @@ class Shellcode:
                     continue
               
                 self.log.debug("[Shellcode] Testing variable: %s" % (name, ))
-                emu.new()
                 
                 try:
                     shellcode = s.decode('utf-8')
@@ -62,12 +89,14 @@ class Shellcode:
                         sc += struct.pack('<H', ord(c))
                 except:
                     continue
-                    
-                offset = emu.shellcode_getpc_test(sc)
-                if offset < 0:
-                     offset = 0
+
+                self.emu.run(sc)
+                if self.emu.emu_profile_output:
+                    self.log.warning(self.emu.emu_profile_output)
+                    libemu = True
+
+                self.emu.free()
                 
-                emu.prepare(sc, offset)
-                emu.test()
-                emu.free()
+                if not libemu:
+                    self.search_url(sc)
 
