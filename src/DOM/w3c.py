a694f9b2d34cb3eb16ccccb029f3dc5306c1c87d
Angelo Dell'Aera <buffer@olografix.org>
2011-08-05 15:37:56 +0200

    Minor fix

--- src/DOM/w3c.py
+++ src/DOM/w3c.py
@@ -824,6 +824,14 @@ class HTMLOptionsCollection(HTMLCollection):
     def __init__(self, doc, nodes):
         HTMLCollection.__init__(self, doc, nodes) 
 
+    def getLength(self):
+        return len(self.nodes)
+
+    def setLength(self):
+        raise DOMException(DOMException.NOT_SUPPORTED_ERR)
+
+    length = property(getLength, setLength)
+
 
 class CSSStyleDeclaration(object):
     def __init__(self, style):
