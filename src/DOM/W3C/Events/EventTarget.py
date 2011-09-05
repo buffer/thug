ed5d91f43a1e1d542a725ebb103c59592dfdf3da
Angelo Dell'Aera <buffer@olografix.org>
2011-09-05 19:25:43 +0200

    New logging subsystem (patch #8)

--- src/DOM/W3C/Events/EventTarget.py
+++ src/DOM/W3C/Events/EventTarget.py
@@ -3,19 +3,19 @@ from __future__ import with_statement
 
 import logging
 
+log = logging.getLogger('Thug.DOM.W3C.Events.EventTarget')
+
 # Introduced in DOM Level 2
 class EventTarget:
-    log = logging.getLogger('EventTarget')
-
     def addEventListener(self, eventType, listener, capture = False):
-        self.log.warning('addEventListener(%s, \n%s, \n%s)' % (eventType, listener, capture, ))
-        print type(listener)
+        log.warning('addEventListener(%s, \n%s, \n%s)' % (eventType, listener, capture, ))
+        listener.__call__()
 
     def removeEventListener(self, eventType, listener, capture = False):
-        self.log.warning('removeEventListener(%s, \n%s, \n%s)' % (eventType, listener, capture, ))
-        print type(listener)
+        log.warning('removeEventListener(%s, \n%s, \n%s)' % (eventType, listener, capture, ))
+        listener.__call__()
 
     def dispatchEvent(self, evt):
-        self.log.warning('dispatchEvent(%s)' % (evt, ))
+        log.warning('dispatchEvent(%s)' % (evt, ))
         return True
 
