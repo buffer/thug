a50073e12d62776997b5387a1b477d64d3164e86
Angelo Dell'Aera <buffer@olografix.org>
2011-09-15 12:57:24 +0200

    Improvements in EnjoySAP ActiveX module

--- src/ActiveX/modules/EnjoySAP.py
+++ src/ActiveX/modules/EnjoySAP.py
@@ -1,4 +1,5 @@
 
+
 acct = ActiveXAcct[self]
 
 def LaunchGui(arg0, arg1, arg2):
@@ -13,5 +14,30 @@ def PrepareToPostHTML(arg):
     if len(arg) > 1000:
         acct.add_alert('EnjoySAP.PrepareToPostHTML overflow in arg0')
 
+def Comp_Download(arg0, arg1):
+    global acct
+
+    acct.add_alert(arg0)
+    acct.add_alert(arg1)
+
+    headers = {
+        'user-agent' : 'Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)'
+    }
+
+    import hashlib, httplib2
+    h = httplib2.Http('/tmp/.cache')
+
+    #FIXME: Relative URLs
+    response, content = h.request(arg0, headers = headers)
+    md5 = hashlib.md5()
+    md5.update(content)
+    filename = md5.hexdigest()
+    acct.add_alert("[*] Saving File: " + filename)
+    
+    with open(filename, 'wb') as fd:
+        fd.write(content)
+
+
 self.LaunchGui         = LaunchGui
 self.PrepareToPostHTML = PrepareToPostHTML
+self.Comp_Download     = Comp_Download
