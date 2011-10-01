c56c1985c5b40cfee606f70e691db924fab220b4
Angelo Dell'Aera <buffer@olografix.org>
2011-10-01 16:11:30 +0200

    Minor changes

--- src/ActiveX/modules/ShockwaveFlash10.py
+++ src/ActiveX/modules/ShockwaveFlash10.py
@@ -5,7 +5,6 @@ def GetVariable(arg):
     global acct
 
     if arg == "$version":
-        acct.add_alert("GetVariable($version)")
         return "WIN 10,0,64,0"
 
 self.GetVariable = GetVariable
--- src/ActiveX/modules/ShockwaveFlash9.py
+++ src/ActiveX/modules/ShockwaveFlash9.py
@@ -5,7 +5,6 @@ def GetVariable(arg):
     global acct
 
     if arg == "$version":
-        acct.add_alert("GetVariable($version)")
         return "WIN 9,0,64,0"
 
 self.GetVariable = GetVariable
