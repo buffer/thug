8f24271c4079c544d55dd981c986204c0b1d3d67
Angelo Dell'Aera <buffer@olografix.org>
2011-10-24 23:56:07 +0200

    Minor changes in MacrovisionFlexNet ActiveX module

--- src/ActiveX/modules/MacrovisionFlexNet.py
+++ src/ActiveX/modules/MacrovisionFlexNet.py
@@ -1,6 +1,8 @@
 # MacrovisionJob, MacrovisionFlexNet
 # CVE-2007-2419, CVE-2007-5660, CVE-2007-6654, CVE-2007-0321, CVE-2007-0328
 
+import os
+import hashlib
 import logging
 log = logging.getLogger("Thug.ActiveX")
 
@@ -17,7 +19,32 @@ def AddFileEx(self, arg0, arg1, arg2, arg3, arg4, arg5, arg6):
         log.warning('Macrovision ActiveX AddFileEx overflow')
 
 def AddFile(self, arg0, arg1):
-    log.warning('Macrovision ActiveX AddFile("%s", "%s")' % (arg0, arg1))
+    log.warning('[Macrovision ActiveX] AddFile("%s", "%s")' % (arg0, arg1))
+    log.warning("[Macrovision ActiveX] Fetching from URL %s" % (arg0, ))
+
+    try:
+        response, content = self._window._navigator.fetch(arg0)
+    except:
+        log.warning('[Macrovision  ActiveX] Fetch failed')
+        return
+
+    if not response or response.status == 404:
+        return 
+
+    md5 = hashlib.md5()
+    md5.update(content)
+    filename = md5.hexdigest()
+
+    log.warning("[Macrovision ActiveX] Saving File: " + filename)
+  
+    baseDir = logging.getLogger("Thug").baseDir
+
+    try:
+        fd = os.open(os.path.join(baseDir, filename), os.O_RDWR | os.O_CREAT)
+        os.write(fd, content)
+        os.close(fd)
+    except:
+        pass
 
 def SetPriority(self, priority):
     log.warning('Macrovision ActiveX SetPriority(%s)' % (priority, ))
