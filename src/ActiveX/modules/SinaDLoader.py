1ab706a768162b6569a08dfa5a3878fe382cbea6
Angelo Dell'Aera <buffer@olografix.org>
2011-10-24 23:56:57 +0200

    Minor changes in SinaDLoader ActiveX modules

--- src/ActiveX/modules/SinaDLoader.py
+++ src/ActiveX/modules/SinaDLoader.py
@@ -1,9 +1,35 @@
 # Sina DLoader Class ActiveX Control 'DonwloadAndInstall' 
 # Method Arbitrary File Download Vulnerability
 
+import os
+import hashlib
 import logging
 log = logging.getLogger("Thug.ActiveX")
 
 def DownloadAndInstall(self, url):
-    log.warning('SinaDLoader Downloader ActiveX Vulnerability (URL: %s)' % (url, )) 
+    log.warning("[SinaDLoader Downloader ActiveX] Fetching from URL %s" % (url, ))
+
+    try:
+        response, content = self._window._navigator.fetch(url)
+    except:
+        log.warning('[SinaDLoader Downloader ActiveX] Fetch failed')
+        return
+
+    if response.status == 404:
+        return 
+
+    md5 = hashlib.md5()
+    md5.update(content)
+    filename = md5.hexdigest()
+
+    log.warning("[SinaDLoader Downloader ActiveX] Saving File: " + filename)
+  
+    baseDir = logging.getLogger("Thug").baseDir
+
+    try:
+        fd = os.open(os.path.join(baseDir, filename), os.O_RDWR | os.O_CREAT)
+        os.write(fd, content)
+        os.close(fd)
+    except:
+        pass
 
