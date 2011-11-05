13ff3cfe3a414e2a3113ac8f2ecee6a2fbcb18b9
Angelo Dell'Aera <buffer@olografix.org>
2011-11-05 16:44:11 +0100

    Minor bug fix in ZenturiProgramCheckerAttack ActiveX module

--- src/ActiveX/modules/ZenturiProgramCheckerAttack.py
+++ src/ActiveX/modules/ZenturiProgramCheckerAttack.py
@@ -9,18 +9,14 @@ log = logging.getLogger("Thug.ActiveX")
 def DownloadFile(self, *arg):
     log.warning('ZenturiProgramChecker ActiveX Attack in DownloadFile function')
 
-    #h = httplib2.Http('/tmp/.cache')
+    log.warning('[ZenturiProgramChecker ActiveX] Downloading from %s' % (arg[0], ))
+    log.warning("[ZenturiProgramChecker ActiveX] Saving downloaded file as: %s" % (arg[1], ))
 
-    #headers = {
-    #    'user-agent' : logging.getLogger("Thug").userAgent,
-    #}
-
-    #FIXME: Relative URLs
     try:
-        #response, content = h.request(arg[0], headers = headers)
-        response, content = self._window._navigator.fecth(arg[0])
+        response, content = self._window._navigator.fetch(arg[0])
     except:
         log.warning('[ZenturiProgramChecker ActiveX] Fetch failed')
+        return
 
     if response.status == 404:
         log.warning("FileNotFoundError: %s" % (url, ))
@@ -30,9 +26,15 @@ def DownloadFile(self, *arg):
     md5.update(content)
     filename = md5.hexdigest()
     log.warning("[ZenturiProgramChecker ActiveX] Saving File: %s" % (filename, ))
-    
-    with open(os.path.join(object._log.baseDir, filename), 'wb') as fd:
-        fd.write(content)
+
+    baseDir = logging.getLogger("Thug").baseDir
+
+    try:
+        fd = os.open(os.path.join(baseDir, filename), os.O_RDWR | os.O_CREAT)
+        os.write(fd, content)
+        os.close(fd)
+    except:
+        pass
 
 def DebugMsgLog(self, *arg):
     log.warning('ZenturiProgramChecker ActiveX Attack in DebugMsgLog function')
