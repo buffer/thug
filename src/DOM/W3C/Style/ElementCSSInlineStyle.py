1c2c9606d18f30cfca1cc4a8e7bbfbecded5520f
Angelo Dell'Aera <buffer@olografix.org>
2011-12-20 17:15:36 +0100

    Minor change

--- src/DOM/W3C/Style/ElementCSSInlineStyle.py
+++ src/DOM/W3C/Style/ElementCSSInlineStyle.py
@@ -4,6 +4,9 @@ from __future__ import with_statement
 from CSSStyleDeclaration import CSSStyleDeclaration
 
 class ElementCSSInlineStyle(object):
+    def __init__(self, doc, tag):
+        self.tag = tag
+
     @property
     def style(self):
         return CSSStyleDeclaration(self.tag['style'] if self.tag.has_key('style') else '')
