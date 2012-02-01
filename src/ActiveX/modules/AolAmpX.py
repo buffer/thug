c2be63b57758705c5e0d0b4a66bb0eaa8502358d
Angelo Dell'Aera <buffer@olografix.org>
2012-02-01 23:20:55 +0100

    MAEC logging support #7

--- src/ActiveX/modules/AolAmpX.py
+++ src/ActiveX/modules/AolAmpX.py
@@ -2,14 +2,14 @@
 # CVE-2007-6250
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def AppendFileToPlayList(self, arg):
     if len(arg) > 512: 
-        log.warning('AOL AmpX overflow in AppendFileToPlayList')
+        log.MAEC.add_behavior_warn('AOL AmpX overflow in AppendFileToPlayList', 'CVE-2007-6250')
 
 def ConvertFile(self, *arg):
     #FIXME
     if len(arg[0]) > 512:
-        log.warning('AOL AmpX overflow in ConvertFile')
+        log.MAEC.add_behavior_warn('AOL AmpX overflow in ConvertFile', 'CVE-2007-6250')
 
