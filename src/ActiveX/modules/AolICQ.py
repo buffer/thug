9136d29edb9b23001ac03e6c8a8a902ba6b0c0ec
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 00:41:48 +0100

    MAEC logging support #9

--- src/ActiveX/modules/AolICQ.py
+++ src/ActiveX/modules/AolICQ.py
@@ -3,28 +3,28 @@
 
 import hashlib
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DownloadAgent(self, url):
-    log.warning('[AOL ICQ ActiveX] Arbitrary File Download and Execute')
-    log.warning('[AOL ICQ ActiveX] Fetching from URL: %s' % (url, ))
+    log.MAEC.add_behavior_warn('[AOL ICQ ActiveX] Arbitrary File Download and Execute')
+    log.MAEC.add_behavior_warn('[AOL ICQ ActiveX] Fetching from URL: %s' % (url, ))
     
     try:
         response, content = self._window._navigator.fetch(url)
     except:
-        log.warning('[AOL ICQ ActiveX] Fetch failed')
+        log.MAEC.add_behavior_warn('[AOL ICQ ActiveX] Fetch failed')
         return
 
     if response.status == 404:
-        log.warning("FileNotFoundError: %s" % (url, ))
+        log.MAEC.add_behavior_warn("FileNotFoundError: %s" % (url, ))
         return 
  
-    baseDir = logging.getLogger("Thug").baseDir
+    baseDir = log.baseDir
 
     md5 = hashlib.md5()
     md5.update(content)
     filename = md5.hexdigest()
-    log.warning("[AOL ICQ ActiveX] Saving File: " + filename)    
+    log.MAEC.add_behavior_warn("[AOL ICQ ActiveX] Saving File: " + filename)    
 
     with open(os.path.join(baseDir, filename), 'wb') as fd: 
         fd.write(content)
