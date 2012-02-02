e9a072a5683763d864f81d5ec7f1debd8999775b
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 11:29:59 +0100

    MAEC logging support #27

--- src/ActiveX/modules/GLIEDown2.py
+++ src/ActiveX/modules/GLIEDown2.py
@@ -1,9 +1,9 @@
 # Ourgame GLWorld GLIEDown2.dll ActiveX Control Vulnerabilities
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def IEStartNative(self, arg0, arg1, arg2):
     if len(arg0) > 220:
-        log.warning('GLWorld GLIEDown2.dll ActiveX IEStartNative Method Buffer Overflow')
+        log.MAEC.add_behavior_warn('GLWorld GLIEDown2.dll ActiveX IEStartNative Method Buffer Overflow')
 
