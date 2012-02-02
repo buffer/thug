7eeed1a31e2da1bfee7b1513c21796b1de5a0ffe
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 11:25:50 +0100

    MAEC logging support #24

--- src/ActiveX/modules/FacebookPhotoUploader.py
+++ src/ActiveX/modules/FacebookPhotoUploader.py
@@ -2,17 +2,17 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def SetExtractIptc(self, val):
     self.__dict__['ExtractIptc'] = val
 
     if len(val) > 255:
-        log.warning('FaceBook PhotoUploader overflow in ExtractIptc property')
+        log.MAEC.add_behavior_warn('FaceBook PhotoUploader overflow in ExtractIptc property')
 
 def SetExtractExif(self, val):
     self.__dict__['ExtractExif'] = val
 
     if len(val) > 255:
-        log.warning('FaceBook PhotoUploader overflow in ExtractExif property')
+        log.MAEC.add_behavior_warn('FaceBook PhotoUploader overflow in ExtractExif property')
 
