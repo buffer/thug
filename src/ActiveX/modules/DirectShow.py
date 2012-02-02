f24cf5109041f520486fce653f6576157b9fdcb3
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 11:16:04 +0100

    MAEC logging support #17

--- src/ActiveX/modules/DirectShow.py
+++ src/ActiveX/modules/DirectShow.py
@@ -2,9 +2,10 @@
 # CVE-2008-0015,CVE-2008-0020
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def Setdata(self, val):
     self.__dict__['data'] = val
-    log.warning('Microsoft DirectShow MPEG2TuneRequest Component Stack Overflow in data property')
+    log.MAEC.add_behavior_warn('Microsoft DirectShow MPEG2TuneRequest Component Stack Overflow in data property',
+                               'CVE-2008-0015')
 
