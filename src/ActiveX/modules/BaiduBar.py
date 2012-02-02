ec2dcc9980d93fb22dd7f0f345e8825589e7c88f
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 10:52:32 +0100

    MAEC logging support #10

--- src/ActiveX/modules/BaiduBar.py
+++ src/ActiveX/modules/BaiduBar.py
@@ -2,8 +2,8 @@
 # BUGTRAQ  ID: 25121
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DloadDS(self, arg0, arg1, arg2):
     if str(arg0).lower().find(".cab") != -1:
-        log.warning('BaiduBar.dll ActiveX DloadDS() function is trying to download %s' % (arg0, ))
+        log.MAEC.add_behavior_warn('BaiduBar.dll ActiveX DloadDS function trying to download %s' % (arg0, ))
