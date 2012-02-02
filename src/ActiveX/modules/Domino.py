2dd57bd9b0c415cb241ed9406001f32f1c0cc204
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 11:18:38 +0100

    MAEC logging support #20

--- src/ActiveX/modules/Domino.py
+++ src/ActiveX/modules/Domino.py
@@ -2,25 +2,28 @@
 # CVE-2007-4474
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def SetGeneral_ServerName(self, val):
     self.__dict__['General_ServerName'] = val
 
     if len(val) > 1024:
-        log.warning('Domino overflow in General_ServerName property')
+        log.MAEC.add_behavior_warn('Domino overflow in General_ServerName property',
+                                   'CVE-2007-4474')
 
 def SetGeneral_JunctionName(self, val):
     self.__dict__['General_JunctionName'] = val
 
     if len(val) > 1024:
-        log.warning('Domino overflow in General_JunctionName property')
+        log.MAEC.add_behavior_warn('Domino overflow in General_JunctionName property',
+                                   'CVE-2007-4474')
 
 def SetMail_MailDbPath(self, val):
     self.__dict__['Mail_MailDbPath'] = val
 
     if len(val) > 1024:
-        log.warning('Domino overflow in Mail_MailDbPath property')
+        log.MAEC.add_behavior_warn('Domino overflow in Mail_MailDbPath property',
+                                   'CVE-2007-4474')
 
 def InstallBrowserHelperDll(self):
     pass
