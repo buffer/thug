381e9f10e26064c7f2058eb2da417d4b867514e0
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 10:52:51 +0100

    MAEC logging support #11

--- src/ActiveX/modules/BitDefender.py
+++ src/ActiveX/modules/BitDefender.py
@@ -2,10 +2,9 @@
 # CVE-2007-5775
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def initx(self, arg):
-
     if len(arg) > 1024:
-        log.warning('BitDefender Online Scanner InitX() overflow')
+        log.MAEC.add_behavior_warn('BitDefender Online Scanner InitX overflow')
 
