bc9c3b1908d3bdf1f64c4019eeaa3e83b8b91f1d
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 11:27:48 +0100

    MAEC logging support #25

--- src/ActiveX/modules/FileUploader.py
+++ src/ActiveX/modules/FileUploader.py
@@ -2,11 +2,11 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def SetHandwriterFilename(self, val):
     self.__dict__['HandwriterFilename'] = val
 
     if len(val) > 1024:
-        log.warning('FileUploader ActiveX overflow in HandwriterFilename property')
+        log.MAEC.add_behavior_warn('FileUploader ActiveX overflow in HandwriterFilename property')
 
