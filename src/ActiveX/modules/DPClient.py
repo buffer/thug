9d37fa5023e4f0c3cb76d705f7555005558df467
Angelo Dell'Aera <buffer@olografix.org>
2012-02-02 11:19:16 +0100

    MAEC logging support #21

--- src/ActiveX/modules/DPClient.py
+++ src/ActiveX/modules/DPClient.py
@@ -2,10 +2,11 @@
 # CVE-2007-5064
 
 import logging
-log = logging.getLogger("ActiveX.Thug")
+log = logging.getLogger("Thug")
 
 def DownURL2(self, arg0, *args):
     if len(arg0) > 1024:
-        log.warning('DPClient.Vod.1 ActiveX Control DownURL2 Method Buffer Overflow')
+        log.MAEC.add_behavior_warn('DPClient.Vod.1 ActiveX Control DownURL2 Method Buffer Overflow',
+                                   'CVE-2007-5064')
 
 
