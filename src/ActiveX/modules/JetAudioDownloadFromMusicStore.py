81f9619aab4523dcb03d2eabfe54790575ac141c
Angelo Dell'Aera <buffer@olografix.org>
2012-02-05 23:22:27 +0100

    MAEC logging support #35

--- src/ActiveX/modules/JetAudioDownloadFromMusicStore.py
+++ src/ActiveX/modules/JetAudioDownloadFromMusicStore.py
@@ -3,27 +3,28 @@
 
 import hashlib
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DownloadFromMusicStore(self, url, dst, title, artist, album, genere, size, param1, param2):
-    log.warning('[JetAudio ActiveX] Downloading from URL %s (saving locally as %s)' % (url, dst, ))
+    log.MAEC.add_behavior_warn('[JetAudio ActiveX] Downloading from URL %s (saving locally as %s)' % (url, dst, ))
 
     try:
         response, content = self._window._navigator.fetch(url)
     except:
-        log.warning('[JetAudio ActiveX] Fetch failed')
+        log.MAEC.add_behavior_warn('[JetAudio ActiveX] Fetch failed')
         return
     
     if response.status == 404:
+        log.MAEC.add_behavior_warn('[JetAudio ActiveX] Fetch failed')
         return 
 
     md5 = hashlib.md5()
     md5.update(content)
     filename = md5.hexdigest()
 
-    log.warning("[JetAudio ActiveX] Saving File: " + filename)
+    log.MAEC.add_behavior_warn("[JetAudio ActiveX] Saving File: " + filename)
   
-    baseDir = logging.getLogger("Thug").baseDir
+    baseDir = log.baseDir
 
     try:
         fd = os.open(os.path.join(baseDir, filename), os.O_RDWR | os.O_CREAT)
