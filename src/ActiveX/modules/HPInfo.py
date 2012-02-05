94dd25647305980582458697cac7a354465c4c87
Angelo Dell'Aera <buffer@olografix.org>
2012-02-05 23:14:44 +0100

    MAEC logging support #30

--- src/ActiveX/modules/HPInfo.py
+++ src/ActiveX/modules/HPInfo.py
@@ -2,23 +2,31 @@
 # CVE-2007-6331, CVE-2007-6332, CVE-2007-6333
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def LaunchApp(self, prog, args, unk):
-    log.warning("HP Info Center LaunchApp called to run: %s %s" % (prog, args, ))
+    log.MAEC.add_behavior_warn("HP Info Center LaunchApp called to run: %s %s" % (prog, args, ), 
+                               "CVE-2007-6331")
 	
 def SetRegValue(self, key, section, keyname, value):
-    log.warning("HP Info Center SetRegValue: %s/%s/%s set to %s" % (str(key), str(section), str(keyname), str(value), ))
+    log.MAEC.add_behavior_warn("HP Info Center SetRegValue: %s/%s/%s set to %s" % (str(key), 
+                                                                                   str(section), 
+                                                                                   str(keyname), 
+                                                                                   str(value), ),
+                               "CVE-2007-6332")
 
 def GetRegValue(self, key, section, keyname):
-    log.warning("HP Info Center GetRegValue, reading: %s/%s/%s" % (key, section, keyname, ))
+    log.MAEC.add_behavior_warn("HP Info Center GetRegValue, reading: %s/%s/%s" % (str(key), 
+                                                                                  str(section), 
+                                                                                  str(keyname), ),
+                               "CVE-2007-6333")
 
 def EvaluateRules(self):
-    log.warning("HP Info Center EvaluateRules")
+    log.MAEC.add_behavior_warn("HP Info Center EvaluateRules")
 
 def SaveToFile(self, path):
-    log.warning("HP Software Update SaveToFile(), writes to %s" % (path, ))
+    log.MAEC.add_behavior_warn("HP Software Update SaveToFile(), writes to %s" % (path, ))
 
 def ProcessRegistryData(self, parm):
-    log.warning("HP Info Center ProcessRegistryData: %s " % (parm, ))
+    log.MAEC.add_behavior_warn("HP Info Center ProcessRegistryData: %s " % (parm, ))
 
