23398f19ce5296afd455f82e9588fadb16f49847
Angelo Dell'Aera <buffer@olografix.org>
2012-02-05 23:17:38 +0100

    MAEC logging support #32

--- src/ActiveX/modules/IMWebControl.py
+++ src/ActiveX/modules/IMWebControl.py
@@ -2,12 +2,14 @@
 # CVE-2007-6493, CVE-2007-6492
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def ProcessRequestEx(self, arg):
     if len(arg) == 0:
-        log.warning('IMWebControl NULL value in ProcessRequestEx()')
+        log.MAEC.add_behavior_warn('IMWebControl NULL value in ProcessRequestEx',
+                                   'CVE-2007-6492')
 
 def SetHandler(self, arg):
     if str([arg]) == '218959117':
-        log.warning('IMWebControl overflow in SetHandler()')
+        log.MAEC.add_behavior_warn('IMWebControl overflow in SetHandler',
+                                   'CVE-2007-6493')
