bed9eb6cf577f5f93eeb8c5b7ffd51d1f92168d0
Angelo Dell'Aera <buffer@olografix.org>
2012-02-05 23:16:48 +0100

    MAEC logging support #31

--- src/ActiveX/modules/ICQToolbar.py
+++ src/ActiveX/modules/ICQToolbar.py
@@ -2,8 +2,8 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def GetPropertyById(self, arg0, arg1):
     if len(arg1) > 120:
-        log.warning('ICQToolbar buffer overflow in GetPropertyById')
+        log.MAEC.add_behavior_warn('ICQToolbar buffer overflow in GetPropertyById')
