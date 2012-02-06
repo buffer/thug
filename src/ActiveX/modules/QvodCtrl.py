f4d6e2e3855eaa4b1921a6a8110c0c3576535ad5
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 20:19:57 +0100

    MAEC logging support #52

--- src/ActiveX/modules/QvodCtrl.py
+++ src/ActiveX/modules/QvodCtrl.py
@@ -2,12 +2,12 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def SetURL(self, val):
     self.__dict__['URL'] = val
     self.__dict__['url'] = val
 
     if len(val) > 800:
-        log.warning('Qvod Player QvodCtrl Class ActiveX Overflow in URL property')
+        log.MAEC.add_behavior_warn('[Qvod Player QvodCtrl Class ActiveX] Overflow in URL property')
 
