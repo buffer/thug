b2ed7d854eff2e707b93b63ad156133317f2d7bd
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 20:20:39 +0100

    MAEC logging support #53

--- src/ActiveX/modules/RDSDataSpace.py
+++ src/ActiveX/modules/RDSDataSpace.py
@@ -2,11 +2,11 @@
 # CVE-2006-0003
 
 import logging
-log = logging.getLogger('Thug.ActiveX')
+log = logging.getLogger("Thug")
 
 def CreateObject(self, object, param = ''):
     import ActiveX
 
-    log.warning("[Microsoft MDAC RDS.Dataspace ActiveX] CreateObject (%s)" % (object))
+    log.MAEC.add_behavior_warn("[Microsoft MDAC RDS.Dataspace ActiveX] CreateObject (%s)" % (object))
     return ActiveX.ActiveX._ActiveXObject(self._window, object)
 
