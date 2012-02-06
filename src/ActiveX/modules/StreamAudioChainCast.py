4ec01d36bed58fed86ceddd10621f5b1df5be188
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 23:00:37 +0100

    MAEC logging support #69

--- src/ActiveX/modules/StreamAudioChainCast.py
+++ src/ActiveX/modules/StreamAudioChainCast.py
@@ -2,8 +2,8 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def InternalTuneIn(self, arg0, arg1, arg2, arg3, arg4):
     if len(arg0) > 248:
-        log.warning('StreamAudio ChainCast ProxyManager buffer overflow in arg0')
+        log.MAEC.add_behavior_warn('[StreamAudio ChainCast VMR Client Proxy ActiveX] Buffer overflow in arg0')
