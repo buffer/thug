48194ef72592d2bf19deeebd2a7641a268b50d9c
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:30:02 +0100

    MAEC logging support #56

--- src/ActiveX/modules/RegistryPro.py
+++ src/ActiveX/modules/RegistryPro.py
@@ -2,11 +2,11 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DeleteKey(self, arg0, arg1):
     if arg0 in (80000001, 80000002, ):
-        log.warning('RegistryPro ActiveX deleting [HKEY_LOCAL_MACHINE/%s]' % (arg1, ))
+        log.MAEC.add_behavior_warn('[RegistryPro ActiveX] Deleting [HKEY_LOCAL_MACHINE/%s]' % (arg1, ))
 	
 def About(self):
-    log.warning('RegistryPro ActiveX About called')
+    log.MAEC.add_behavior_warn('[RegistryPro ActiveX] About called')
