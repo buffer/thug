572a92bf2b2d1dc52a546a04b02039b98d4ad606
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 23:29:33 +0100

    MAEC logging support #83

--- src/ActiveX/modules/YahooJukebox.py
+++ src/ActiveX/modules/YahooJukebox.py
@@ -2,17 +2,17 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def AddBitmap(self, arg0, arg1, arg2, arg3, arg4, arg5):
     if len(arg1) > 256:
-        log.warning('Yahoo Jukebox ActiveX Overflow in AddBitmap')
+        log.MAEC.add_behavior_warn('[Yahoo! Music Jukebox ActiveX] Overflow in AddBitmap')
 
 def AddButton(self, arg0, arg1):
     if len(arg0) > 256:
-        log.warning('Yahoo Jukebox ActiveX Overflow in AddButton')
+        log.MAEC.add_behavior_warn('[Yahoo! Music Jukebox ActiveX] Overflow in AddButton')
 
 def AddImage(self, arg0, arg1):
     if len(arg0) > 256:
-        log.warning('Yahoo Jukebox ActiveX Overflow in AddImage')
+        log.MAEC.add_behavior_warn('[Yahoo! Music Jukebox ActiveX] Overflow in AddImage')
 
