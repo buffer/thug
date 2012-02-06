fd9bd81b193c874eaaf2e5a530922dad28ff3033
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:34:08 +0100

    MAEC logging support #59

--- src/ActiveX/modules/ScriptingFileSystemObject.py
+++ src/ActiveX/modules/ScriptingFileSystemObject.py
@@ -1,19 +1,19 @@
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def OpenTextFile(self, sFilePathAndName, ForWriting = True, flag = True):
-    log.warning('[Script.FileSystemObject ActiveX] OpenTextFile("%s", "%s", "%s")' % (sFilePathAndName, ForWriting, flag))
+    log.MAEC.add_behavior_warn('[Script.FileSystemObject ActiveX] OpenTextFile("%s", "%s", "%s")' % (sFilePathAndName, ForWriting, flag))
 
 def Write(self, sFileContents):
-    log.warning('[Script.FileSystemObject ActiveX] Write("%s")' % (sFileContents, ))
+    log.MAEC.add_behavior_warn('[Script.FileSystemObject ActiveX] Write("%s")' % (sFileContents, ))
 
 def Close(self):
-    log.warning('[Script.FileSystemObject ActiveX] Close()')
+    log.MAEC.add_behavior_warn('[Script.FileSystemObject ActiveX] Close()')
 
 def BuildPath(self, arg0, arg1):
-    log.warning('[Script.FileSystemObject ActiveX] BuildPath("%s", "%s")' % (arg0, arg1, ))
+    log.MAEC.add_behavior_warn('[Script.FileSystemObject ActiveX] BuildPath("%s", "%s")' % (arg0, arg1, ))
 
 def GetSpecialFolder(self, arg):
-    log.warning('[Script.FileSystemObject ActiveX] GetSpecialFolder(%s)' % (arg, ))
+    log.MAEC.add_behavior_warn('[Script.FileSystemObject ActiveX] GetSpecialFolder(%s)' % (arg, ))
     return '%TEMP%'
