9334f23a5c0fa63f8b470f303a6b00d6bdebfc31
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:32:42 +0100

    MAEC logging support #57

--- src/ActiveX/modules/RisingScanner.py
+++ src/ActiveX/modules/RisingScanner.py
@@ -2,8 +2,8 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def UpdateEngine(self):
-    log.warning('Rising Scanner ActiveX UpdateEngine Method vulnerability' )
+    log.MAEC.add_behavior_warn('[Rising Online Virus Scanner Web Scan ActiveX] UpdateEngine Method vulnerability' )
 
