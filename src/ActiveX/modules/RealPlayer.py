4a8423408316b6e50292cc333743fd8b686f4a34
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:28:20 +0100

    MAEC logging support #54

--- src/ActiveX/modules/RealPlayer.py
+++ src/ActiveX/modules/RealPlayer.py
@@ -2,26 +2,30 @@
 # CVE-2007-5601
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DoAutoUpdateRequest(self, arg0, arg1, arg2):
     if len(arg0) > 1000 or len(arg1) > 1000:
-        log.warning('RealPlayer 10.5 ierpplug.dll overflow in DoAutoUpdateRequest')
+        log.MAEC.add_behavior_warn('[RealMedia RealPlayer Ierpplug.DLL ActiveX] Overflow in DoAutoUpdateRequest',
+                                   'CVE-2007-5601')
 
 def PlayerProperty(self, arg):
     if arg == 'PRODUCTVERSION':
         return '6.0.14.552'
 
     if len(arg) > 1000:
-        log.warning('RealPlayer 10.5 ierpplug.dll overflow in PlayerProperty')
+        log.MAEC.add_behavior_warn('[RealMedia RealPlayer Ierpplug.DLL ActiveX] Overflow in PlayerProperty',
+                                   'CVE-2007-5601')
 
 def Import(self, arg):
     if len(arg) > 0x8000:
-        log.warning('RealPlayer 10.5 ierpplug.dll overflow in Import')
+        log.MAEC.add_behavior_warn('[RealMedia RealPlayer Ierpplug.DLL ActiveX] Overflow in Import',
+                                   'CVE-2007-5601')
 
 def SetConsole(self, val):
     self.__dict__['Console'] = val
 
     if len(val) >= 32:
-        log.warning('RealPlayer rmoc3260.dll overflow in Console property')
+        log.MAEC.add_behavior_warn('[RealMedia RealPlayer rmoc3260.DLL ActiveX] Overflow in Console property',
+                                   'CVE-2007-5601')
 
