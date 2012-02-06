466fa53bd768286650202f04411a7024d52ce177
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 23:26:22 +0100

    MAEC logging support #79

--- src/ActiveX/modules/WinZip.py
+++ src/ActiveX/modules/WinZip.py
@@ -2,8 +2,9 @@
 # CVE-2006-3890,CVE-2006-5198,CVE-2006-6884
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def CreateNewFolderFromName(self, arg):
     if len(arg) > 230:
-        log.warning('WinZip ActiveX CreateNewFolderFromName Overflow')
+        log.MAEC.add_behavior_warn('[WinZip ActiveX] CreateNewFolderFromName Overflow',
+                                   'CVE-2006-6884')
