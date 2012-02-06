31932c00129a9407492371bf0feab374a6978009
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 23:23:23 +0100

    MAEC logging support #75

--- src/ActiveX/modules/VLC.py
+++ src/ActiveX/modules/VLC.py
@@ -2,17 +2,20 @@
 # CVE-2007-4619, CVE-2007-6262
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def getVariable(self, arg):
     if len(arg) > 255:
-        log.warning('VLC ActiveX getVariable Overflow')
+        log.MAEC.add_behavior_warn('[VLC ActiveX] getVariable Overflow',
+                                   'CVE-2007-6262')
 
 def setVariable(self, arg0, arg1):
     if len(arg0) > 255 or len(arg1) > 255:
-        log.warning('VLC ActiveX setVariable Overflow')
+        log.MAEC.add_behavior_warn('[VLC ActiveX] setVariable Overflow',
+                                   'CVE-2007-6262')
 
 def addTarget(self, arg0, arg1, arg2, arg3):
     if len(arg0) > 255 or len(arg1) > 255 or len(arg2) > 255 or len(arg3) > 255:
-        log.warning('VLC ActiveX addTarget Overflow')
+        log.MAEC.add_behavior_warn('[VLC ActiveX] addTarget Overflow',
+                                   'CVE-2007-6262')
 
