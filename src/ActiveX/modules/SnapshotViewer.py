b31526dc65852cd2c2fb94936b311e6069518ea0
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:53:31 +0100

    MAEC logging support #63

--- src/ActiveX/modules/SnapshotViewer.py
+++ src/ActiveX/modules/SnapshotViewer.py
@@ -6,8 +6,7 @@ import httplib2
 import hashlib
 import logging
 
-log = logging.getLogger("Thug.ActiveX")
-
+log = logging.getLogger("Thug")
 
 def PrintSnapshot(self, SnapshotPath = '', CompressedPath = ''):
     if SnapshotPath:
@@ -16,27 +15,28 @@ def PrintSnapshot(self, SnapshotPath = '', CompressedPath = ''):
     if CompressedPath:
         self.CompressedPath = CompressedPath
 
-    log.warning('[Microsoft Access Snapshot Viewer ActiveX] SnapshotPath : %s, CompressedPath: %s' % (self.SnapshotPath, self.CompressedPath, ))
-
+    msg = '[Microsoft Access Snapshot Viewer ActiveX] SnapshotPath : %s, CompressedPath: %s' % (self.SnapshotPath, 
+                                                                                                self.CompressedPath, )
+    log.MAEC.add_behavior_warn(msg, 'CVE-2008-2463')
     url = self.SnapshotPath
 
     try:
         response, content = self._window._navigator.fetch(url)
     except:
-        log.warning('[Microsoft Access Snapshot Viewer ActiveX] Fetch failed')
+        log.MAEC.add_behavior_warn('[Microsoft Access Snapshot Viewer ActiveX] Fetch failed')
         return
 
     if response.status == 404:
-        log.warning("FileNotFoundError: %s" % (url, ))
+        log.MAEC.add_behavior_warn('[Microsoft Access Snapshot Viewer ActiveX] FileNotFoundError: %s' % (url, ))
         return 
  
-    baseDir = logging.getLogger("Thug").baseDir
+    baseDir = log.baseDir
 
     md5 = hashlib.md5()
     md5.update(content)
     filename = md5.hexdigest()
 		
-    log.warning("[Microsoft Access Snapshot Viewer ActiveX] Saving File: " + filename)
+    log.MAEC.add_behavior_warn("[Microsoft Access Snapshot Viewer ActiveX] Saving File: " + filename)
     with open(os.path.join(baseDir, filename), 'wb') as fd:
         fd.write(content)
 	
