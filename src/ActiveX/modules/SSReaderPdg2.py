1ceba4740d16274c153167b4806a7920ca9d3607
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:56:38 +0100

    MAEC logging support #66

--- src/ActiveX/modules/SSReaderPdg2.py
+++ src/ActiveX/modules/SSReaderPdg2.py
@@ -2,13 +2,15 @@
 # CVE-2007-5892
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def Register(self, arg0, arg1):
     if len(arg1) > 255:
-        log.warning('SSReader Pdg2 ActiveX Register Method Overflow')
+        log.MAEC.add_behavior_warn('[SSReader Pdg2 ActiveX] Register Method Overflow',
+                                   'CVE-2007-5892')
 
 def LoadPage(self, arg0, arg1, arg2, arg3):
     if len(arg0) > 255:
-        log.warning('SSReader Pdg2 ActiveX LoadPage Method Overflow')
+        log.MAEC.add_behavior_warn('[SSReader Pdg2 ActiveX] LoadPage Method Overflow',
+                                   'CVE-2007-5892')
 
