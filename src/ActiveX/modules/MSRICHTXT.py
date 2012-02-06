04ff4f67e382a4d9ac012c7e31b0293e6fa24273
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 19:57:41 +0100

    MAEC logging support #41

--- src/ActiveX/modules/MSRICHTXT.py
+++ src/ActiveX/modules/MSRICHTXT.py
@@ -2,9 +2,9 @@
 # CVE-NOMATCH
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def SaveFile(self, path, arg):
-    log.warning("Microsoft Rich Textbox Control ActiveX writing to file % s" % (str(path), ))
-    log.warning("Content: \n%s" % (str(self.Text), ))
+    log.MAEC.add_behavior_warn("[Microsoft Rich Textbox Control ActiveX] Writing to file % s" % (str(path), ))
+    log.MAEC.add_behavior_warn("[Microsoft Rich Textbox Control ActiveX] Content: \n%s" % (str(self.Text), ))
 
