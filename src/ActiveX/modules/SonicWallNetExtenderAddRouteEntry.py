6025793572d5059687ae366cf44c4ad00972e35d
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:54:21 +0100

    MAEC logging support #64

--- src/ActiveX/modules/SonicWallNetExtenderAddRouteEntry.py
+++ src/ActiveX/modules/SonicWallNetExtenderAddRouteEntry.py
@@ -2,12 +2,11 @@
 # CVE-2007-5603 (AddRouteEntry)
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def AddRouteEntry(self, arg0, arg1):
-    if len(arg0) > 20:
-        log.warning('SonicWall SSL-VPN NetExtender NELaunchCtrl ActiveX Overflow in AddRouteEntry arg0')
-    if len(arg1) > 20:
-        log.warning('SonicWall SSL-VPN NetExtender NELaunchCtrl ActiveX Overflow in AddRouteEntry arg1')
+    if len(arg0) > 20 or len(arg1) > 20:
+        log.MAEC.add_behavior_warn('[SonicWall SSL-VPN NetExtender NELaunchCtrl ActiveX] Overflow in AddRouteEntry',
+                                   'CVE-2007-5603')
 
 	
