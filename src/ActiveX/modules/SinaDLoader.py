8e088d7f750ad66d1e85c13151995f15e2c2ef55
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 22:52:25 +0100

    MAEC logging support #62

--- src/ActiveX/modules/SinaDLoader.py
+++ src/ActiveX/modules/SinaDLoader.py
@@ -4,27 +4,28 @@
 import os
 import hashlib
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DownloadAndInstall(self, url):
-    log.warning("[SinaDLoader Downloader ActiveX] Fetching from URL %s" % (url, ))
+    log.MAEC.add_behavior_warn("[SinaDLoader Downloader ActiveX] Fetching from URL %s" % (url, ))
 
     try:
         response, content = self._window._navigator.fetch(url)
     except:
-        log.warning('[SinaDLoader Downloader ActiveX] Fetch failed')
+        log.MAEC.add_behavior_warn('[SinaDLoader Downloader ActiveX] Fetch failed')
         return
 
     if response.status == 404:
+        log.MAEC.add_behavior_warn("[SinaDLoader Downloader ActiveX] FileNotFoundError: %s" % (url, ))
         return 
 
     md5 = hashlib.md5()
     md5.update(content)
     filename = md5.hexdigest()
 
-    log.warning("[SinaDLoader Downloader ActiveX] Saving File: " + filename)
+    log.MAEC.add_behavior_warn("[SinaDLoader Downloader ActiveX] Saving File: " + filename)
   
-    baseDir = logging.getLogger("Thug").baseDir
+    baseDir = log.baseDir
 
     try:
         fd = os.open(os.path.join(baseDir, filename), os.O_RDWR | os.O_CREAT)
