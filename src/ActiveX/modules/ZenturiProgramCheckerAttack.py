55846826b689f5851374387d5a5f8a3f7b500cd6
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 23:31:54 +0100

    MAEC logging support #86

--- src/ActiveX/modules/ZenturiProgramCheckerAttack.py
+++ src/ActiveX/modules/ZenturiProgramCheckerAttack.py
@@ -4,30 +4,30 @@ import hashlib
 import httplib2
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DownloadFile(self, *arg):
-    log.warning('ZenturiProgramChecker ActiveX Attack in DownloadFile function')
+    log.MAEC.add_behavior_warn('[ZenturiProgramChecker ActiveX] Attack in DownloadFile function')
 
-    log.warning('[ZenturiProgramChecker ActiveX] Downloading from %s' % (arg[0], ))
-    log.warning("[ZenturiProgramChecker ActiveX] Saving downloaded file as: %s" % (arg[1], ))
+    log.MAEC.add_behavior_warn('[ZenturiProgramChecker ActiveX] Downloading from %s' % (arg[0], ))
+    log.MAEC.add_behavior_warn("[ZenturiProgramChecker ActiveX] Saving downloaded file as: %s" % (arg[1], ))
 
     try:
         response, content = self._window._navigator.fetch(arg[0])
     except:
-        log.warning('[ZenturiProgramChecker ActiveX] Fetch failed')
+        log.MAEC.add_behavior_warn('[ZenturiProgramChecker ActiveX] Fetch failed')
         return
 
     if response.status == 404:
-        log.warning("FileNotFoundError: %s" % (url, ))
+        log.MAEC.add_behavior_warn("[ZenturiProgramChecker ActiveX] FileNotFoundError: %s" % (url, ))
         return 
 
     md5 = hashlib.md5()
     md5.update(content)
     filename = md5.hexdigest()
-    log.warning("[ZenturiProgramChecker ActiveX] Saving File: %s" % (filename, ))
+    log.MAEC.add_behavior_warn("[ZenturiProgramChecker ActiveX] Saving File: %s" % (filename, ))
 
-    baseDir = logging.getLogger("Thug").baseDir
+    baseDir = log.baseDir
 
     try:
         fd = os.open(os.path.join(baseDir, filename), os.O_RDWR | os.O_CREAT)
@@ -37,8 +37,8 @@ def DownloadFile(self, *arg):
         pass
 
 def DebugMsgLog(self, *arg):
-    log.warning('ZenturiProgramChecker ActiveX Attack in DebugMsgLog function')
+    log.MAEC.add_behavior_warn('[ZenturiProgramChecker ActiveX] Attack in DebugMsgLog function')
 
 def NavigateUrl(self, *arg):
-    log.warning('ZenturiProgramChecker ActiveX Attack in NavigateUrl function')
+    log.MAEC.add_behavior_warn('[ZenturiProgramChecker ActiveX] Attack in NavigateUrl function')
 
