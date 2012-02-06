86e1010676be0e69ddb2b159c190a817c8b4faa5
Angelo Dell'Aera <buffer@olografix.org>
2012-02-06 20:16:34 +0100

    MAEC logging support #49

--- src/ActiveX/modules/PPlayer.py
+++ src/ActiveX/modules/PPlayer.py
@@ -1,21 +1,21 @@
 # Xunlei Thunder PPLAYER.DLL_1.WORK ActiveX Control
 
 import logging
-log = logging.getLogger("Thug.ActiveX")
+log = logging.getLogger("Thug")
 
 def DownURL2(self, arg0, arg1, arg2, arg3):
     if len(arg0) > 1024:
-        log.warning('Xunlei Thunder 5.x DownURL2 Overflow')
+        log.MAEC.add_behavior_warn('[Xunlei Thunder PPlayer ActiveX] DownURL2 Overflow')
 
 def SetFlvPlayerUrl(self, val):
     self.__dict__['FlvPlayerUrl'] = val
 
     if len(val) > 1060:
-        log.warning('Xunlei Thunder XPPlayer Class FlvPlayerUrl Property Handling Buffer Overflow')
+        log.MAEC.add_behavior_warn('[Xunlei Thunder PPlayer ActiveX] FlvPlayerUrl Property Handling Buffer Overflow')
 
 def SetLogo(self, val):
     self.__dict__['Logo'] = val
 
     if len(val) > 128:
-        log.warning('PPStream (PowerPlayer.dll 2.0.1.3829) ActiveX Remote Overflow Exploit in Logo property')
+        log.MAEC.add_behavior_warn('[Xunlei Thunder PPlayer ActiveX] Remote Overflow Exploit in Logo property')
 
