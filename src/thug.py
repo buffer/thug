58ebf4563a1fbefb8887dbba76d0f2bea15790be
Angelo Dell'Aera <buffer@olografix.org>
2012-05-02 16:50:42 +0200

    Version 0.2.9

--- src/thug.py
+++ src/thug.py
@@ -31,7 +31,7 @@ from DOM.Personality import Personality
 from DOM import Window, DFT
 from Logging.ThugLogging import ThugLogging
 
-__thug_version__ = '0.2.8'
+__thug_version__ = '0.2.9'
 
 log = logging.getLogger("Thug")
 log.setLevel(logging.WARN)
