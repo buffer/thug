25604c1866a8a365eb781f605e34c8930dfb5404
Angelo Dell'Aera <buffer@olografix.org>
2012-05-23 23:52:31 +0200

    Added browser version attribute to personalities

--- src/DOM/Personality.py
+++ src/DOM/Personality.py
@@ -25,6 +25,7 @@ class Personality(dict):
         self['xpie60'] = {
                 "id"              : 1,
                 "description"     : "Internet Explorer 6.0 (Windows XP)",
+                "version"         : "6.0",
                 "userAgent"       : "Mozilla/4.0 (Windows;  MSIE 6.0;  Windows NT 5.1;  SV1; .NET CLR 2.0.50727)",
                 "appCodeName"     : "Mozilla",
                 "appName"         : "Microsoft Internet Explorer",
@@ -37,6 +38,7 @@ class Personality(dict):
         self['xpie61'] = { 
                 "id"              : 2,
                 "description"     : "Internet Explorer 6.1 (Windows XP)", 
+                "version"         : "6.1",
                 "userAgent"       : "Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
                 "appCodeName"     : "Mozilla",
                 "appName"         : "Microsoft Internet Explorer",
@@ -49,6 +51,7 @@ class Personality(dict):
         self['xpie70'] = {
                 "id"              : 3,
                 "description"     : "Internet Explorer 7.0 (Windows XP)",
+                "version"         : "7.0",
                 "userAgent"       : "Mozilla/4.0 (Windows; MSIE 7.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)",
                 "appCodeName"     : "Mozilla",
                 "appName"         : "Microsoft Internet Explorer",
@@ -61,6 +64,7 @@ class Personality(dict):
         self['xpie80'] = {
                 "id"              : 4,
                 "description"     : "Internet Explorer 8.0 (Windows XP)",
+                "version"         : "8.0",
                 "userAgent"       : "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; (R1 1.5); .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
                 "appCodeName"     : "Mozilla",
                 "appName"         : "Microsoft Internet Explorer",
@@ -74,6 +78,7 @@ class Personality(dict):
         self['w2kie60'] = {
                 "id"              : 5,
                 "description"     : "Internet Explorer 6.0 (Windows 2000)",
+                "version"         : "6.0",
                 "userAgent"       : "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
                 "appCodeName"     : "Mozilla",
                 "appName"         : "Microsoft Internet Explorer",
@@ -86,6 +91,7 @@ class Personality(dict):
         self['w2kie80'] = {
                 "id"              : 6,
                 "description"     : "Internet Explorer 8.0 (Windows 2000)",
+                "version"         : "8.0",
                 "userAgent"       : "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.0; Trident/4.0; InfoPath.1; SV1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 3.0.04506.30)",
                 "appCodeName"     : "Mozilla",
                 "appName"         : "Microsoft Internet Explorer",
@@ -94,6 +100,10 @@ class Personality(dict):
                 "platform"        : "Win32",
                 "browserTag"      : "ie80",
                 }
+
+    @property
+    def browserVersion(self):
+        return self[log.ThugOpts.useragent]['version']
 
     def isIE(self):
         return self[log.ThugOpts.useragent]['appName'] == "Microsoft Internet Explorer"
