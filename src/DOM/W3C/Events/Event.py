96e2c4614ef7856eb9a88a54b40f75507e22e27e
Angelo Dell'Aera <buffer@olografix.org>
2012-06-08 10:36:22 +0200

    Events handling/emulation (patch #2)

--- src/DOM/W3C/Events/Event.py
+++ src/DOM/W3C/Events/Event.py
@@ -1,53 +1,82 @@
 #!/usr/bin/env python
 
+import logging
+
+log = logging.getLogger("Thug")
+
 # Introduced in DOM Level 2
+
 class Event:
     CAPTURING_PHASE     = 1 # The current event phase is the capturing phase.
     AT_TARGET           = 2 # The event is currently being evaluated at the target EventTarget
     BUBBLING_PHASE      = 3 # The current event phase is the bubbling phase.
 
-    def __init__(self, evt, target):
-        self._evt          = evt
-        self._target       = target
-        self.currentTarget = target
-        self.eventPhase    = self.AT_TARGET
-        self._stop         = False
-        self._prevent      = False
+    def __init__(self, target):
+        self._target             = target
+        self.currentTarget       = target
+        self.eventPhase          = self.AT_TARGET
+        self._stoppedPropagation = False
+        self._defaultPrevented   = False
+        self._canBubble          = False
+        self._cancelable         = False
+
+        # Prior to IE9, IE does not support the stopPropagation() method. Instead, 
+        # the IE Event object has a property named `cancelBubble'. Setting this 
+        # property to true prevents any further propagation (IE8 and before do not 
+        # support the captuting phase of event propagation so bubbling is the only
+        # kind of propagation to be canceled)
+        if log.ThugOpts.Personality.isIE() and log.ThugOpts.Personality.browserVersion < '9.0':
+            self.cancelBubble = property(self._getPropagationStatus, self._setPropagationStatus)
+        else:
+            self.stopPropagation = self._stopPropagation
+
+        # In IE prior to IE9 the default action can be canceled by setting the
+        # `returnValue' of the Event object to false
+        if log.ThugOpts.Personality.isIE() and log.ThugOpts.Personality.browserVersion < '9.0':
+            self.returnValue = property(self._getDefaultPrevented, self._setDefaultPrevented)
+        else:
+            self.preventDefault = self._preventDefault
+
+    def _getPropagationStatus(self):
+        return self._stoppedPropagation
+
+    def _setPropagationStatus(self, value):
+        self._stoppedPropagation = True if value else False
+
+    def _stopPropagation(self):
+        self._stoppedPropagation = True
+
+    def _getDefaultPrevented(self):
+        return self._defaultPrevented
+
+    def _setDefaultPrevented(self, value):
+        self._defaultPrevented = True if value else False
+
+    def _preventDefault(self):
+        self._defaultPrevented = True
 
     @property
     def type(self):
-        return self._evt
+        return self._type
 
     @property
     def target(self):
         return self._target
 
-    #@property
-    #def currentTarget(self):
-    #    return None
-
-    #@property
-    #def eventPhase(self):
-    #    pass
-
     @property
     def bubbles(self):
-        return False
+        return self._canBubble
 
     @property
     def cancelable(self):
-        return False
+        return self._cancelable
 
     @property
     def timeStamp(self):
         return 0
 
-    def stopPropagation(self):
-        self._stop = True
-
-    def preventDefault(self):
-        self._prevent = True
-
     def initEvent(self, eventTypeArg, canBubbleArg, cancelableArg):
-        pass
+        self._type       = eventTypeArg
+        self._canBubble  = canBubbleArg
+        self._cancelable = cancelableArg
 
