6bb49d0621b8ce47a9491c53be291453ee96c8c1
Angelo Dell'Aera <buffer@olografix.org>
2012-06-08 10:39:24 +0200

    Events handling/emulation (patch #6)

---
+++ src/DOM/W3C/Events/MutationEvent.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Introduced in DOM Level 2
+class MutationEvent(Event):
+    MODIFICATION = 1 # The Attr was just added
+    ADDITION     = 2 # The Attr was modified in place
+    REMOVAL      = 3 # The Attr was just removed
+
+    @property
+    def relatedNode(self):
+        return None
+
+    @property 
+    def prevValue(self):
+        return None
+
+    @property
+    def newValue(self):
+        return None
+
+    @property
+    def attrName(self):
+        return None
+
+    @property
+    def attrChange(self):
+        return None
+
+    def initMutationEvent(self, typeArg,
+                                canBubbleArg,
+                                cancelableArg,
+                                relatedNodeArg,
+                                prevValueArg,
+                                newValueArg,
+                                attrNameArg,
+                                attrChangeArg):
+        pass
