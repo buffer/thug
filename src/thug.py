98b0e152be91458555120a43df6b6b661b92aa47
Angelo Dell'Aera <buffer@olografix.org>
2012-06-08 10:49:58 +0200

    Version 0.4.0

--- src/thug.py
+++ src/thug.py
@@ -32,7 +32,7 @@ from DOM import Window, DFT
 from Logging.ThugLogging import ThugLogging
 from Plugins.ThugPlugins import *
 
-__thug_version__ = '0.3.1'
+__thug_version__ = '0.4.0'
 
 log = logging.getLogger("Thug")
 log.setLevel(logging.WARN)
