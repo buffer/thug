3e57cbe4c1bb96d965e8c4e37f8a9bc62de78650
Angelo Dell'Aera <buffer@olografix.org>
2012-06-20 11:12:48 +0200

    HTMLElement innerHTML handling

--- src/DOM/W3C/HTML/HTMLElement.py
+++ src/DOM/W3C/HTML/HTMLElement.py
@@ -9,10 +9,16 @@ except:
     from StringIO import StringIO
 
 import bs4 as BeautifulSoup
+import logging
+
+from ActiveX.ActiveX import _ActiveXObject
 from Element import Element
 from Style.ElementCSSInlineStyle import ElementCSSInlineStyle
 from .attr_property import attr_property
 from .text_property import text_property
+
+log = logging.getLogger("Thug")
+
 
 class HTMLElement(Element, ElementCSSInlineStyle):
     id              = attr_property("id")
@@ -21,8 +27,7 @@ class HTMLElement(Element, ElementCSSInlineStyle):
     dir             = attr_property("dir")
     className       = attr_property("class")
 
-    @property
-    def innerHTML(self):
+    def getInnerHTML(self):
         if not self.hasChildNodes():
             return ""
 
@@ -33,16 +38,33 @@ class HTMLElement(Element, ElementCSSInlineStyle):
 
         return html.getvalue()
 
-    @innerHTML.setter
-    def innerHTML(self, html):
+    def setInnerHTML(self, html):
+        soup = BeautifulSoup.BeautifulSoup(html, "html5lib")
         # FIXME
-        #dom = BeautifulSoup.BeautifulSoup(html)
-        dom = BeautifulSoup.BeautifulSoup(html, "html5lib")
-        # FIXME
-        self.tag.contents = []
-
-        for node in dom.contents:
+        #self.tag.contents = []
+        
+        for node in soup.body.children:
             self.tag.append(node)
+
+        for node in self.tag.descendants:
+            name = getattr(node, 'name', None)
+            if not name:
+                continue
+        
+            p = getattr(self, '_handle_%s' % (name, ), None)
+            if p:
+                p(node)
+            
+    innerHTML = property(getInnerHTML, setInnerHTML)
+
+    def _handle_object(self, object):
+        log.warning(object)
+                            
+        classid = object.get('classid', None)
+        id      = object.get('id', None)
+
+        if classid and id: 
+            setattr(self.doc.window, id, _ActiveXObject(self.window, classid, 'id'))
 
     # WARNING: NOT DEFINED IN W3C SPECS!
     def focus(self):
