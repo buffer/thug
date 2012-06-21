998da3990b7f41668efa26ef57458d528409d5c6
Angelo Dell'Aera <buffer@olografix.org>
2012-06-21 16:46:31 +0200

    Move appendChild, replaceChild, removeChild, insertBefore from W3C DOM Element to W3C DOM Node (as stated in DOM2-Core)

--- src/DOM/W3C/Node.py
+++ src/DOM/W3C/Node.py
@@ -114,24 +114,142 @@ class Node(PyV8.JSClass, EventTarget):
     @property
     def ownerDocument(self):
         return self.doc
+
+    def findChild(self, child):
+        try:
+            return self.tag.contents.index(child.tag)
+        except:
+            return -1
+
+    def is_readonly(self, node):
+        return node.nodeType in (Node.DOCUMENT_TYPE_NODE,
+                                 Node.NOTATION_NODE,
+                                 Node.ENTITY_REFERENCE_NODE,
+                                 Node.ENTITY_NODE, )
+
+    def is_text(self, node):
+        return node.nodeType in (Node.TEXT_NODE,
+                                 Node.PROCESSING_INSTRUCTION_NODE,
+                                 Node.CDATA_SECTION_NODE,
+                                 Node.COMMENT_NODE, )
     
     def insertBefore(self, newChild, refChild):
-        raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+        if not newChild:
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+        
+        if not isinstance(newChild, Node):
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
 
-    def insertAfter(self, newChild, refChild):
-        raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+        # If refChild is null, insert newChild at the end of the list 
+        # of children
+        if not refChild:
+            return self.appendChild(newChild)
+
+        if not isinstance(refChild, Node):
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+
+        index = self.findChild(refChild)
+        if index < 0 and not self.is_text(refChild):
+            raise DOMException(DOMException.NOT_FOUND_ERR)
+
+        # If the newChild is already in the tree, it is first removed
+        if getattr(newChild, 'tag', None) and newChild.tag in self.tag.contents:
+            self.tag.contents.remove(newChild.tag)
+
+        if self.is_text(newChild):
+            self.tag.insert(index, newChild.data.output_ready())
+            return newChild
+
+        if newChild.nodeType in (Node.DOCUMENT_FRAGMENT_NODE, ):
+            self.tag.insert(index, newChild.tag.findChild())
+            return newChild
+
+        self.tag.insert(index, newChild.tag)
+        return newChild
 
     def replaceChild(self, newChild, oldChild):
-        raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
-    
+        # NO_MODIFICATION_ALLOWED_ERR: Raised if this node or the parent of 
+        # the new node is readonly.
+        if self.is_readonly(self):
+            raise DOMException(DOMException.NO_MODIFICATION_ALLOWED)
+
+        parent = getattr(newChild, 'parentNode', None)
+        if parent:
+            if self.is_readonly(parent):
+                raise DOMException(DOMException.NO_MODIFICATION_ALLOWED)
+
+        if not newChild or not oldChild:
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+
+        if not isinstance(newChild, Node) or not isinstance(oldChild, Node):
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+
+        index = self.findChild(oldChild)
+        if index < 0 and not self.is_text(refChild):
+            raise DOMException(DOMException.NOT_FOUND_ERR)
+
+        if self.is_text(newChild):
+            self.tag.contents[index] = newChild.data.output_ready()
+            return oldChild
+
+        if newChild.nodeType in (Node.DOCUMENT_FRAGMENT_NODE, ):
+            self.tag.contents[index] = newChild.tag.findChild()
+            return oldChild
+
+        self.tag.contents[index] = newChild.tag
+        return oldChild
+
     def removeChild(self, oldChild):
-        raise DOMException(DOMException.NOT_FOUND_ERR)
-    
+        # NO_MODIFICATION_ALLOWED_ERR: Raised if this node is readonly
+        if self.is_readonly(self):
+            raise DOMException(DOMException.NO_MODIFICATION_ALLOWED)
+
+        if not oldChild:
+            raise DOMException(DOMException.NOT_FOUND_ERR)
+
+        if not isinstance(oldChild, Node):
+            raise DOMException(DOMException.NOT_FOUND_ERR)
+
+        index = self.findChild(oldChild)
+        if index < 0 and not self.is_text(oldChild):
+            raise DOMException(DOMException.NOT_FOUND_ERR)
+
+        if getattr(oldChild, 'tag', None) and oldChild.tag in self.tag.contents:
+            self.tag.contents.remove(oldChild.tag)
+
+        return oldChild
+
     def appendChild(self, newChild):
-        raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
-    
+        # NO_MODIFICATION_ALLOWED_ERR: Raised if this node is readonly
+        if self.is_readonly(self):
+            raise DOMException(DOMException.NO_MODIFICATION_ALLOWED)
+
+        if self.is_text(self):
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+
+        if not newChild:
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+
+        if not isinstance(newChild, Node):
+            raise DOMException(DOMException.HIERARCHY_REQUEST_ERR)
+
+        # If the newChild is already in the tree, it is first removed
+        if getattr(newChild, 'tag', None) and newChild.tag in self.tag.contents:
+            self.tag.contents.remove(newChild.tag)
+
+        if self.is_text(newChild):
+            self.tag.append(newChild.data.output_ready())
+            return newChild
+
+        if newChild.nodeType in (Node.DOCUMENT_FRAGMENT_NODE, ):
+            self.tag.append(newChild.tag.findChild())
+            return newChild
+
+        self.tag.append(newChild.tag)
+        return newChild
+
     def hasChildNodes(self):
-        return False
+        return len(self.tag.contents) > 0
 
     # Modified in DOM Level 2
     def normalize(self):
