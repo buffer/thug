cbed7a137b870cb47ed3fef555bf4d83421cc7df
Angelo Dell'Aera <buffer@olografix.org>
2012-07-16 23:35:52 +0200

    Version 0.4.6

--- src/thug.py
+++ src/thug.py
@@ -32,7 +32,7 @@ from DOM import Window, DFT, MIMEHandler
 from Logging.ThugLogging import ThugLogging
 from Plugins.ThugPlugins import *
 
-__thug_version__ = '0.4.5'
+__thug_version__ = '0.4.6'
 
 log = logging.getLogger("Thug")
 log.setLevel(logging.WARN)
