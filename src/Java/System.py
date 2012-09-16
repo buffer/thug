25670788a91f1e276c93215557d8165419dca5f1
Angelo Dell'Aera <buffer@olografix.org>
2012-09-16 22:44:07 +0200

    Java System getProperty minor fix

--- src/Java/System.py
+++ src/Java/System.py
@@ -1,11 +1,16 @@
 
+import logging
+log = logging.getLogger("Thug")
+
 class System:
     def __init__(self):
         pass
 
     def getProperty(self, property):
         if property == "java.version":
-            return '1.6.1_15'
+            javaplugin = log.ThugVulnModules._javaplugin.split('.')
+            last       = javaplugin.pop()
+            return '%s_%s' % ('.'.join(javaplugin), last)
 
         if property == "java.vendor":
             return 'Sun Microsystems Inc.'
