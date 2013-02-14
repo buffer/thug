9b13dd9be3ded0835a376629171a2446bd4ded71
Angelo Dell'Aera <buffer@olografix.org>
2013-02-14 09:31:49 +0100

    DOM W3C HTML - HTMLTableRowElement improvements

--- src/DOM/W3C/HTML/HTMLTableRowElement.py
+++ src/DOM/W3C/HTML/HTMLTableRowElement.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
+import logging
+from DOMException import DOMException
 from .HTMLElement import HTMLElement
+from .HTMLCollection import HTMLCollection
+from .HTMLTableCellElement import HTMLTableCellElement
 from .attr_property import attr_property
+
+log = logging.getLogger("Thug")
 
 class HTMLTableRowElement(HTMLElement):
     def __init__(self, doc, tag):
         HTMLElement.__init__(self, doc, tag)
+        self._cells = HTMLCollection(doc, list())
 
     # Modified in DOM Level 2
     @property
@@ -20,7 +27,7 @@ class HTMLTableRowElement(HTMLElement):
     # Modified in DOM Level 2
     @property
     def cells(self):
-        raise NotImplementedError()
+        return self._cells
 
     align           = attr_property("align")
     bgColor         = attr_property("bgcolor")
@@ -29,10 +36,32 @@ class HTMLTableRowElement(HTMLElement):
     vAlign          = attr_property("valign")
 
     # Modified in DOM Level 2
-    def insertCell(self, index):
-        pass
+    def insertCell(self, index = None):
+        # `index' specifies the position of the row to insert (starts at 0).
+        # The value of -1 can also be used; which result in that the new row
+        # will be inserted at the last position. This parameter is required
+        # in Firefox and Opera, but optional in Internet Explorer, Chrome and
+        # Safari. If this parameter is omitted, insertRow() inserts a new row
+        # at the last position in IE and at the first position in Chrome and
+        # Safari.
+        if index is None:
+            if log.ThugOpts.Personality.isIE():
+                index = -1
+            if log.ThugOpts.Personality.isChrome() or log.ThugOpts.Personality.isSafari():
+                index = 0
+
+        cell = HTMLTableCellElement(self.doc, self.tag, index)
+
+        if index in (-1, len(self._cells), ):
+            self.cells.nodes.append(cell)
+        else:
+            self.cells.nodes.insert(index, cell)
+
+        return cell
 
     # Modified in DOM Level 2
     def deleteCell(self, index):
-        pass
+        if index < -1 or index >= len(self.cells.nodes):
+            raise DOMException(DOMException.INDEX_SIZE_ERR)
 
+        del self.cells.nodes[index]
