b9ee2e0050ea0b1cf9432e697ac518253a23be19
Angelo Dell'Aera <buffer@olografix.org>
2013-02-25 11:48:10 +0100

    Logging Mapper pydot support and minor changes

--- src/Logging/Mapper.py
+++ src/Logging/Mapper.py
@@ -16,13 +16,18 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 #
-# Provided by Thorsten Sick <thorsten.sick@avira.com> from Avira
-# For the iTES Project http://ites-project.org
+# Author:   Thorsten Sick <thorsten.sick@avira.com> from Avira
+#           (developed for the iTES Project http://ites-project.org)
+#
+# Changes:
+#           - pydot support
+#             Angelo Dell'Aera <angelo.dellaera@honeynet.org>
 
 import os
-from subprocess import call
 import json
 import fnmatch
+import pydot
+
 try:
     import urlib.parse as urlparse
 except ImportError:
@@ -39,8 +44,7 @@ class DictDiffer(object):
     """
     def __init__(self, current_dict, past_dict):
         self.current_dict, self.past_dict = current_dict, past_dict
-        self.set_current, self.set_past =\
-            set(current_dict.keys()), set(past_dict.keys())
+        self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
         self.intersect = self.set_current.intersection(self.set_past)
 
     def added(self):
@@ -50,48 +54,56 @@ class DictDiffer(object):
         return self.set_past - self.intersect
 
     def changed(self):
-        return set(o for o in self.intersect if self.past_dict[o] !=\
-            self.current_dict[o])
+        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
 
     def unchanged(self):
-        return set(o for o in self.intersect if self.past_dict[o] ==\
-            self.current_dict[o])
+        return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
 
     def anychange(self):
         if len(self.added()):
             return True
+
         if len(self.removed()):
             return True
+
         if len(self.changed()):
             return True
+
         return False
 
 
 class Mapper():
-    """ Map URL relationships
+    """
+        Map URL relationships
     """
 
-    def __init__(self, resdir, simplify=False):
-        """
+    markup_types = ("text/html", "text/xml", "text/css" )
+    image_types  = ("image/", )
+    exec_types   = ("application/javascript", "text/javascript", "application/x-javascript")
 
-        @resdir: Directory to store the result svg in
-        @simplify: Reduce the urls to server names
+    def __init__(self, resdir, simplify = False):
         """
-        self.resdir = resdir
+        @resdir     : Directory to store the result svg in
+        @simplify   : Reduce the urls to server names
+        """
+        self.resdir   = resdir
         self.simplify = simplify
-        self.dotfile = os.path.join(self.resdir, "avdot.dot")
-        self.dfh = None
+        self.dotfile  = os.path.join(self.resdir, "avdot.dot")
+        self.dfh      = None
+
         self._write_start()
         self.data = {"locations": [], "connections": []}
 
     def _write_start(self):
-        """ Write the dot header
+        """
+            Write the dot header
         """
         self.dfh = open(self.dotfile, "w")
         self.dfh.write('digraph {\nrankdir="LR";\ngraph[fontsize=10]\n')
 
     def _write_stop(self):
-        """ Write the dot footer
+        """
+            Write the dot footer
         """
 
         if self.dfh:
@@ -100,56 +112,75 @@ class Mapper():
             self.dfh.close()
             self.dfh = None
 
+    def _check_content_type(self, loc, t):
+        return loc["content-type"] and loc["content-type"].lower().startswith(t)
+
+    def _check_types(self, loc, _types):
+        for t in _types:
+            if self._check_content_type(loc, t):
+                return True
+
+        return False
+
+    def check_markup(self, loc):
+        return self._check_types(loc, self.markup_types)
+
+    def check_image(self, loc):
+        return self._check_types(loc, self.image_types)
+
+    def check_exec(self, loc):
+        return self._check_types(loc, self.exec_types)
+
     def _dot_from_data(self):
         # Create dot from data
         if "locations" in self.data:
             for loc in self.data["locations"]:
-                shape = None
+                shape     = None
                 fillcolor = None
+
                 # Markup
-                for a in ["text/html", "text/xml", "text/css"]:
-                    if (loc["content-type"] and
-                        loc["content-type"].lower().startswith(a)):
-                        shape = "box"
+                if self.check_markup(loc):
+                    shape = "box"
+
                 # Images
-                for a in ["image/"]:
-                    if (loc["content-type"] and
-                        loc["content-type"].lower().startswith(a)):
-                        shape = "oval"
+                if self.check_image(loc):
+                    shape = "oval"
+
                 # Executable stuff
-                for a in ["application/javascript",
-                          "text/javascript",
-                          "application/x-javascript"]:
-                    if (loc["content-type"] and
-                        loc["content-type"].lower().startswith(a)):
-                        shape = "hexagon"
+                if self.check_exec(loc):
+                    shape = "hexagon"
 
                 if "error" in loc["flags"]:
                     fillcolor = "orange"
 
                 self.dfh.write('"%s" [label="%s"' % (loc["url"], loc["url"]))
+
                 if shape:
                     self.dfh.write("shape = %s," % shape)
                 if fillcolor:
-                    self.dfh.write("style = filled, fillcolor = %s,"\
-                        % fillcolor)
+                    self.dfh.write("style = filled, fillcolor = %s," % fillcolor)
+
                 self.dfh.write("]\n")
 
         if "connections" in self.data:
             # Add edges
             for con in self.data["connections"]:
                 color = None
+
                 if "method" in ["iframe"]:
                     color = "orange"
 
                 self.dfh.write('"%s" -> "%s" [label="%s",' %\
                     (con["source"], con["destination"], con["method"]))
+
                 if color:
                     self.dfh.write("color = %s," % color)
+
                 self.dfh.write("]\n")
 
     def _add_to_loc(self, loc):
-        """ Add location information to location data
+        """
+            Add location information to location data
         """
         if self.simplify:
             url = urlparse.urlparse(loc["url"]).netloc
@@ -160,15 +191,18 @@ class Mapper():
             d = DictDiffer(a, loc)
             if not d.anychange():
                 return
+
         self.data["locations"].append(loc)
 
     def _add_to_con(self, con):
-        """ Add connection information to connection data
+        """
+            Add connection information to connection data
         """
         if self.simplify:
             url = urlparse.urlparse(con["source"]).netloc
             if url:
                 con["source"] = url
+
             url = urlparse.urlparse(con["destination"]).netloc
             if url:
                 con["destination"] = url
@@ -190,7 +224,8 @@ class Mapper():
                 self._add_to_con(con)
 
     def add_file(self, filename):
-        """ Add data file
+        """
+            Add data file
         """
         try:
             self.add_data(json.load(open(filename, "r")))
@@ -198,50 +233,57 @@ class Mapper():
             pass
 
     def write_svg(self):
-        """ Create SVG out of the dotfile
-
-        @dotfile: In-dotfile
         """
+            Create SVG out of the dotfile
 
+            @dotfile: In-dotfile
+        """
         self._write_stop()
-        svgfile = os.path.join(self.resdir, "map.svg")
-        cmd = ["dot", "-Tsvg", self.dotfile, "-o", svgfile]
-        res = call(cmd)
+
+        graph = pydot.graph_from_dot_data(open(self.dotfile).read())
+        svg   = graph.create_svg()
+
+        with open(os.path.join(self.resdir, "map.svg"), 'a+r') as fd:
+            fd.write(svg)
 
 
-def allFiles(root, patterns="*", single_level=False, yield_folders=False):
-    """ Walk files
+def allFiles(root, patterns = "*", single_level = False, yield_folders = False):
+    """
+        Walk files
     """
 
     patterns = patterns.split(";")
     for path, subdirs, files in os.walk(root):
         if yield_folders:
             files.extend(subdirs)
+
         files.sort()
+
         for name in files:
             for pattern in patterns:
                 if fnmatch.fnmatch(name, pattern):
                     yield os.path.join(path, name)
                     break
+
         if single_level:
             break
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(
-        description='Receives jobs and starts Thug to process them')
-    parser.add_argument('--resdir', help='Result dir', default=".")
-    parser.add_argument('--source', help='Source file or dir',
-        default="avlog.json")
-    parser.add_argument('--simplify', help='Reduce the URLs to servernames',
-        default=False, action="store_true")
+
+    parser = argparse.ArgumentParser(description = 'Receives jobs and starts Thug to process them')
+    parser.add_argument('--resdir', help = 'Result dir', default = ".")
+    parser.add_argument('--source', help = 'Source file or dir', default = "avlog.json")
+    parser.add_argument('--simplify', help = 'Reduce the URLs to servernames', default = False, action = "store_true")
 
     args = parser.parse_args()
-    m = Mapper(args.resdir, simplify=args.simplify)
+
+    m = Mapper(args.resdir, simplify = args.simplify)
     if os.path.isdir(args.source):
         for afile in allFiles(args.source, "avlog.json"):
             print afile
             m.add_file(afile)
     else:
         m.add_file(args.source)
+
     m.write_svg()
