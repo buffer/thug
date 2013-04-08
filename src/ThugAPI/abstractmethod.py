1d10b6bc7328d7d2bf9ef343bccd334093f47e1e
Angelo Dell'Aera <buffer@olografix.org>
2013-04-08 22:55:43 +0200

    Python 3 compatibility

--- src/ThugAPI/abstractmethod.py
+++ src/ThugAPI/abstractmethod.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import sys
+
 class abstractmethod(object):
     def __init__(self, func):
         self.func = func
 
     def __call__(self, *args, **kwds):
-        raise NotImplementedError("method %s is abstract." % self.func.func_name)
+        func_name = self.func.__name__ if sys.version_info.major >= 3 else self.func.func_name
+        raise NotImplementedError("method %s is abstract." % func_name)
