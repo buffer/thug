96e6df25e72565f881c3ff6ff699942f31f5fc55
Angelo Dell'Aera <buffer@olografix.org>
2013-05-09 17:03:37 +0200

    Convert Thug classifier to Yara rules (patch 3)

--- src/Classifier/URLClassifier.py
+++ src/Classifier/URLClassifier.py
@@ -19,81 +19,28 @@
 # Original code written by Thorsten Sick <thorsten.sick@avira.com> 
 # from Avira (developed for the iTES Project http://ites-project.org)
 #
-# Modified by Angelo Dell'Aera: Designed the more generic Classifier 
-# module and embedded this module into such module
+# Modified by Angelo Dell'Aera:
+#   - Designed the more generic Classifier module and embedded this
+#     module into such module
+#   - Converted to YARA rules
 
 import os
-import re
 import logging
-import json
 from .BaseClassifier import BaseClassifier
 
 log = logging.getLogger("Thug")
 
 class URLClassifier(BaseClassifier):
-    default_rule_file = "urlclassifier.json"
+    default_rule_file = "rules/urlclassifier.yar"
+    classifier        = "URL Classifier"
 
-    def __init__(self, rules = [], verified_only = False):
+    def __init__(self):
         BaseClassifier.__init__(self)
-        self.verified_only = verified_only
-
-        for rule in rules:
-            self.add_rule(rule)
-
-    def add_rule(self, rule_file):
-        if not os.path.exists(rule_file):
-            log.warn("[URL Classifier] Skipping not existing classification rule file %s" % (rule_file, ))
-            return
-
-        with open(rule_file, "rt") as fh:
-            rules = json.load(fh)
-
-            for rule in rules["rules"]:
-                rule["compiled"] = re.compile(rule["regex"], re.IGNORECASE)
-                self.rules.append(rule)
 
     def classify(self, url):
-        for rule in self.rules:
-            if self.verified_only and not rule["verified"]:
-                continue
+        for match in self.rules.match(data = url):
+            self.matches.append((url, match))
 
-            if rule["compiled"].search(url):
-                self.matches.append((url, rule))
-                log.ThugLogging.add_behavior_warn("[URL Classifier] URL: %s Exploit kit: %s (rule: %s)" % (url, 
-                                                                                                           rule["kit"],
-                                                                                                           rule["name"], ))
-
-    def print_classification(self):
-        if not self.matches:
-            return
-
-        for match in self.matches:
-            url  = match[0]
-            rule = match[1]
-            log.warn("[URL Classifier] URL: %s Exploit kit: %s (rule: %s)" % (url, 
-                                                                              rule["kit"], 
-                                                                              rule["name"], ))
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description = "Classifies a given URL")
-    parser.add_argument('url', 
-                        help    = "The URL to classify")
-    
-    parser.add_argument("--rules", 
-                        help    = "Additional rule files (default: urlclassifier.json", 
-                        nargs   = "+", 
-                        default = [])
-
-    parser.add_argument("--verified", 
-                        help    = "Only report verified data", 
-                        action  = "store_true", 
-                        default = False)
-
-    args = parser.parse_args()
-
-    classifier = URLClassifier(args.rules, args.verified)
-    classifier.classify(args.url)
-    classifier.print_classification()
+            rule = " ".join(match.rule.split('_'))
+            tags = ", ".join([" ".join(t.split('_')) for t in match.tags])
+            log.ThugLogging.add_behavior_warn("[URL Classifier] URL: %s (Rule: %s, Classification: %s)" % (url, rule, tags, ))
