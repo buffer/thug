b64498b1d174700befdaea95221abf9f22c6492e
Angelo Dell'Aera <buffer@olografix.org>
2013-06-04 17:58:33 +0200

    Minor change

--- src/DOM/W3C/Node.py
+++ src/DOM/W3C/Node.py
@@ -183,7 +183,7 @@ class Node(PyV8.JSClass, EventTarget):
             raise DOMException(DOMException.NOT_FOUND_ERR)
 
         if self.is_text(newChild):
-            self.tag.insert(index, newChild.data.output_ready())
+            self.tag.insert(index, newChild.data.output_ready(formatter = lambda x: x))
             return newChild
 
         if newChild.nodeType in (Node.DOCUMENT_FRAGMENT_NODE, ):
@@ -225,7 +225,7 @@ class Node(PyV8.JSClass, EventTarget):
             raise DOMException(DOMException.NOT_FOUND_ERR)
 
         if self.is_text(newChild):
-            self.tag.contents[index] = newChild.data.output_ready()
+            self.tag.contents[index] = newChild.data.output_ready(formatter = lambda x: x)
             return oldChild
 
         if newChild.nodeType in (Node.DOCUMENT_FRAGMENT_NODE, ):
@@ -300,7 +300,7 @@ class Node(PyV8.JSClass, EventTarget):
             #self.tag.contents.remove(newChild.tag)
 
         if self.is_text(newChild):
-            self.tag.append(newChild.data.output_ready())
+            self.tag.append(newChild.data.output_ready(formatter = lambda x: x))
             return newChild
 
         if newChild.nodeType in (Node.DOCUMENT_FRAGMENT_NODE, ):
