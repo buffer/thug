183294a1fc643e88f0ced1cb31445db26d7bf08b
Angelo Dell'Aera <buffer@olografix.org>
2013-12-15 23:35:15 +0100

    Minor change

--- src/DOM/W3C/Events/DocumentEvent.py
+++ src/DOM/W3C/Events/DocumentEvent.py
@@ -3,7 +3,8 @@ from DOMException import DOMException
 
 # Introduced in DOM Level 2
 class DocumentEvent:
+    def __init__(self, doc):
+        self.doc = doc
+
     def createEvent(self, eventType):
         raise DOMException(DOMException.NOT_SUPPORTED_ERR)
-
-
