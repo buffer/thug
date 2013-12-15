0ab6ea7b29b8750b48f9c20a6979b20f944c907b
Angelo Dell'Aera <buffer@olografix.org>
2013-12-15 23:36:13 +0100

    HTMLAllCollection interface
    	Obsolete. Represents a collection of all HTML elements within a
    	Document or an HTMLElement.

---
+++ src/DOM/W3C/HTML/HTMLAllCollection.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from .HTMLCollection import HTMLCollection
+from NodeList import NodeList
+
+class HTMLAllCollection(HTMLCollection):
+    def __init__(self, doc, nodes):
+        HTMLCollection.__init__(self, doc, nodes)
+
+    def tags(self, name):
+        s = [p for p in self.doc.find_all(name.lower())]
+        return NodeList(self.doc, s)
