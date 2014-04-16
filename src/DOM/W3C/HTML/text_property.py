d8748b66c72abd88bd4af9d62154e639f3682f85
Angelo Dell'Aera <buffer@olografix.org>
2014-04-16 14:51:06 +0200

    text_property bug fix

--- src/DOM/W3C/HTML/text_property.py
+++ src/DOM/W3C/HTML/text_property.py
@@ -4,15 +4,16 @@ import bs4 as BeautifulSoup
 
 def text_property(readonly = False):
     def getter(self):
-        return str(self.tag.string)
+        return str(self.tag.string) if self.tag.string else "" 
     
     def setter(self, text):
-        if self.tag.string:
-            self.tag.contents[0] = BeautifulSoup.NavigableString(text)
-        else:
-            self.tag.append(text)
-                    
-        self.tag.string = self.tag.contents[0]
+        #if self.tag.string:
+        #    self.tag.contents[0] = BeautifulSoup.NavigableString(text)
+        #else:
+        #   self.tag.append(text)
+        #
+        #self.tag.string = self.tag.contents[0]
+        self.tag.string = text
         
     return property(getter) if readonly else property(getter, setter)
 
