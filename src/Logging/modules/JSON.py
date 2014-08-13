5aa74ccf3a37bb00cfba64dac9b787b1f070bbc2
Angelo Dell'Aera <buffer@olografix.org>
2014-08-14 00:07:01 +0200

    Minor fix

--- src/Logging/modules/JSON.py
+++ src/Logging/modules/JSON.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# JSONLog.py
+# JSON.py
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
