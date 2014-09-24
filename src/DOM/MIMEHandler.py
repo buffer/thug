b6d1d2919f40903223df0ff6e5355fb9effdc4fa
Angelo Dell'Aera <buffer@olografix.org>
2014-09-24 14:01:15 +0200

    ThugLogging store_content - patch 3

--- src/DOM/MIMEHandler.py
+++ src/DOM/MIMEHandler.py
@@ -590,9 +590,7 @@ class MIMEHandler(dict):
         m.update(content)
         md5sum = m.hexdigest()
 
-        rfile = os.path.join(log.ThugLogging.baseDir, md5sum)
-        with open(rfile, 'wb') as fd: 
-            fd.write(content)
+        rfile = log.ThugLogging.store_content(log.ThugLogging.baseDir, md5sum, content)
 
         pdfparser = PDFParser()
 
