5f4bc699ed70747bbcd9f5da31fbfe644976f0ff
Angelo Dell'Aera <buffer@olografix.org>
2015-09-29 16:59:48 +0200

    Python 3 compatibility change

--- src/WebTracking/WebTracking.py
+++ src/WebTracking/WebTracking.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
-from Cookies import Cookies
+from .Cookies import Cookies
 
 import logging
 log = logging.getLogger("Thug")
