8537117988a3a2b4f9a722f7ce6d7ec1d9f03738
Angelo Dell'Aera <buffer@olografix.org>
2015-09-30 11:54:24 +0200

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import glob
+from setuptools import setup, find_packages
+
+from thug import configuration_path
+
+personalities_path = os.path.join(configuration_path, "personalities") 
+rules_path         = os.path.join(configuration_path, "rules")
+js_rules_path      = os.path.join(rules_path, "jsclassifier")
+url_rules_path     = os.path.join(rules_path, "urlclassifier")
+sample_rules_path  = os.path.join(rules_path, "sampleclassifier")
+
+setup(
+    name = "thug",
+    version = "0.6.5",
+    author = "Angelo Dell'Aera",
+    author_email = "buffer@antifork.org",
+    description = "Low-interaction honeyclient Thug",
+    long_description = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "README.md")).read(),
+    package_data = {
+           "DOM"       : ["*.js"],
+           "Debugger"  : ["*.js"],
+           },
+    packages = find_packages(),
+    scripts = ["thug.py", ],
+    data_files = [
+        (configuration_path, ["Analysis/honeyagent/honeyagent.conf.sample",
+                              "Analysis/virustotal/virustotal.conf.default",
+                              "Logging/logging.conf.default",
+                              "Plugins/plugins.conf.default"]),
+        (personalities_path, glob.glob("DOM/personalities/*.json")),
+        (rules_path        , glob.glob("Classifier/rules/*.yar")),
+        (js_rules_path     , glob.glob("Classifier/rules/jsclassifier/*.yar")),
+        (url_rules_path    , glob.glob("Classifier/rules/urlclassifier/*.yar")),
+        (sample_rules_path , glob.glob("Classifier/rules/sampleclassifier/*.yar")),
+    ],
+    install_requires = ["distribute"],
+    entry_points = {
+        "console_scripts": [
+            "thug = thug:main",
+        ]
+    }
+)
