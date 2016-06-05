21a371be5eed29f5322b926e824708c73e497956
Angelo Dell'Aera <buffer@olografix.org>
2016-06-06 00:31:37 +0200

    Minor indentation fix

--- src/ActiveX/modules/HPInfo.py
+++ src/ActiveX/modules/HPInfo.py
@@ -5,59 +5,58 @@ import logging
 log = logging.getLogger("Thug")
 
 def LaunchApp(self, prog, args, unk):
-    log.ThugLogging.add_behavior_warn("[HP Info Center ActiveX] LaunchApp called to run: %s %s" % (prog, args, ), 
-                                      "CVE-2007-6331")
+    log.ThugLogging.add_behavior_warn("[HP Info Center ActiveX] LaunchApp called to run: %s %s" % (prog, args, ), "CVE-2007-6331")
     log.ThugLogging.log_exploit_event(self._window.url,
                                       "HP Info Center ActiveX",
                                       "LaunchApp called to run",
                                       cve = 'CVE-2007-6331',
                                       forward = False,
                                       data = {
-                                                "command": prog,
-                                                "args"   : args
-                                             }
+                                          "command": prog,
+                                          "args"   : args
+                                      }
                                      )
-	
+
 def SetRegValue(self, key, section, keyname, value):
     log.ThugLogging.add_behavior_warn("[HP Info Center ActiveX] SetRegValue: %s/%s/%s set to %s" % (str(key), 
-                                                                                   str(section), 
-                                                                                   str(keyname), 
-                                                                                   str(value), ),
+                                                                                                    str(section), 
+                                                                                                    str(keyname), 
+                                                                                                    str(value), ),
                                       "CVE-2007-6332")
+
     log.ThugLogging.log_exploit_event(self._window.url,
                                       "HP Info Center ActiveX",
                                       "SetRegValue",
                                       cve = 'CVE-2007-6332',
                                       forward = False,
                                       data = {
-                                                "key"    : str(key),
-                                                "section": str(section),
-                                                "keyname": str(keyname),
-                                                "value"  : str(value)
-                                             }
+                                          "key"    : str(key),
+                                          "section": str(section),
+                                          "keyname": str(keyname),
+                                          "value"  : str(value)
+                                      }
                                      )
 
 def GetRegValue(self, key, section, keyname):
     log.ThugLogging.add_behavior_warn("[HP Info Center ActiveX] GetRegValue, reading: %s/%s/%s" % (str(key), 
-                                                                                  str(section), 
-                                                                                  str(keyname), ),
+                                                                                                   str(section), 
+                                                                                                   str(keyname), ),
                                       "CVE-2007-6333")
+
     log.ThugLogging.log_exploit_event(self._window.url,
                                       "HP Info Center ActiveX",
                                       "GetRegValue",
                                       cve = 'CVE-2007-6333',
                                       forward = False,
                                       data = {
-                                                "key": str(key),
-                                                "section": str(section),
-                                                "keyname": str(keyname)
-                                             }
+                                          "key": str(key),
+                                          "section": str(section),
+                                          "keyname": str(keyname)
+                                      }
                                      )
 
 def EvaluateRules(self):
-    log.ThugLogging.log_exploit_event(self._window.url,
-                                      "HP Info Center ActiveX",
-                                      "EvaluateRules")
+    log.ThugLogging.log_exploit_event(self._window.url, "HP Info Center ActiveX", "EvaluateRules")
 
 def SaveToFile(self, path):
     log.ThugLogging.add_behavior_warn("[HP Info Center ActiveX] SaveToFile(), writes to %s" % (path, ))
@@ -65,8 +64,8 @@ def SaveToFile(self, path):
                                       "HP Info Center ActiveX",
                                       "SaveToFile",
                                       data = {
-                                                "filename": path
-                                             },
+                                          "filename": path
+                                      },
                                       forward = False)
 
 def ProcessRegistryData(self, parm):
@@ -75,6 +74,6 @@ def ProcessRegistryData(self, parm):
                                       "HP Info Center ActiveX",
                                       "ProcessRegistryData",
                                       data = {
-                                                "param": parm
-                                             },
+                                          "param": parm
+                                      },
                                       forward = False)
