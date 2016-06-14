fa2a32337258cbb26096864c153771ac5af07e86
Angelo Dell'Aera <buffer@olografix.org>
2016-06-14 11:50:04 +0200

    Minor fix

--- src/ActiveX/modules/MSXML2DOMDocument.py
+++ src/ActiveX/modules/MSXML2DOMDocument.py
@@ -3,7 +3,7 @@ import logging
 log = logging.getLogger("Thug")
 
 def definition(arg):
-    log.ThugLogging.log_exploit_event(self._window.url, #pylint:disable=undefined-variable
+    log.ThugLogging.log_exploit_event(self._window.url, #pylint:disable=undefined-name
                                       "MSXML2.DOMDocument",
                                       "Microsoft XML Core Services MSXML Uninitialized Memory Corruption",
                                       cve = "CVE-2012-1889")
