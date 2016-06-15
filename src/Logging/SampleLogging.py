2a697a876cb20c3837f5461de428ffc9f04ea2be
Angelo Dell'Aera <buffer@olografix.org>
2016-06-15 09:32:41 +0200

    Code cleanup

--- src/Logging/SampleLogging.py
+++ src/Logging/SampleLogging.py
@@ -24,14 +24,6 @@ import hashlib
 import zipfile
 import pefile
 
-try:
-    from io import StringIO
-except ImportError:
-    try:
-        from cStringIO import StringIO
-    except ImportError:
-        from StringIO import StringIO
-
 SSDEEP = True
 
 try:
