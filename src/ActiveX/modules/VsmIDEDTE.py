b3bb0b0a379d7333bb08ceae4072f93e805f1d2f
Angelo Dell'Aera <buffer@antifork.org>
2016-06-23 08:49:42 +0200

    Code cleanup

--- src/ActiveX/modules/VsmIDEDTE.py
+++ src/ActiveX/modules/VsmIDEDTE.py
@@ -2,17 +2,17 @@
 import logging
 log = logging.getLogger("Thug")
 
-def CreateObject(self, object, param = ''):
+def CreateObject(self, _object, param = ''):
     import ActiveX
 
-    log.ThugLogging.add_behavior_warn("[VsmIDE.DTE ActiveX] CreateObject (%s)" % (object))
+    log.ThugLogging.add_behavior_warn("[VsmIDE.DTE ActiveX] CreateObject (%s)" % (_object))
     log.ThugLogging.log_exploit_event(self._window.url,
                                       "VsmIDE.DTE ActiveX",
                                       "CreateObject",
                                       data = {
-                                                "object": object
+                                                "object": _object
                                              },
                                       forward = False)
 
-    return ActiveX.ActiveX._ActiveXObject(self._window, object)
+    return ActiveX.ActiveX._ActiveXObject(self._window, _object)
 
