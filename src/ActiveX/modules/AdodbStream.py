8b2c8d03ca51bcc7cf5964cb3eaf7a5b0d083527
Angelo Dell'Aera <buffer@olografix.org>
2016-06-28 10:46:54 +0200

    ActiveX - AdodbStream improvements

--- src/ActiveX/modules/AdodbStream.py
+++ src/ActiveX/modules/AdodbStream.py
@@ -19,7 +19,7 @@ def Write(self, s):
     self.fobject.write(unicode(s))
 
 def SaveToFile(self, filename, opt = 0):
-    log.ThugLogging.add_behavior_warn("[Adodb.Stream ActiveX] SaveToFile (%s)" % (filename, ))
+    log.ThugLogging.add_behavior_warn("[Adodb.Stream ActiveX] SaveToFile(%s, %s)" % (filename, opt, ))
     log.ThugLogging.log_exploit_event(self._window.url,
                                       "Adodb.Stream ActiveX",
                                       "SaveToFile",
@@ -29,13 +29,28 @@ def SaveToFile(self, filename, opt = 0):
                                       forward = False)
 
     content = self.fobject.getvalue()
-    mtype = Magic(content).get_mime()
+    mtype   = Magic(content).get_mime()
 
-    log.ThugLogging.log_file(
-        content,
-        url=filename,
-        sampletype=mtype,
-    )
+    log.ThugLogging.log_file(content, url = filename, sampletype = mtype)
+    self._files[filename] = content
+
+def LoadFromFile(self, filename):
+    log.ThugLogging.add_behavior_warn("[Adodb.Stream ActiveX] LoadFromFile(%s)" % (filename, ))
+    if filename not in self._files:
+        raise TypeError()
+
+    self._current = filename
+
+def ReadText(self, NumChars = -1):
+    log.ThugLogging.add_behavior_warn("[Adodb.Stream ActiveX] ReadText")
+
+    if NumChars == -1:
+        return self._files[self._current]
+
+    return self._files[self._current][:NumChars - 1]
+
+def WriteText(self, data, options = None):
+    log.ThugLogging.add_behavior_warn("[Adodb.Stream ActiveX] WriteText(%s)" % (data, ))
 
 def Close(self):
     log.ThugLogging.add_behavior_warn("[Adodb.Stream ActiveX] Close")
