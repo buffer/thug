97c3427cec00cbc9ab5a07fac0ea67fbfa4b7920
Angelo Dell'Aera <buffer@olografix.org>
2016-09-14 14:41:21 +0200

    Minor change

--- setup.py
+++ setup.py
@@ -15,15 +15,13 @@ js_rules_path      = os.path.join(rules_path, "jsclassifier")
 url_rules_path     = os.path.join(rules_path, "urlclassifier")
 sample_rules_path  = os.path.join(rules_path, "sampleclassifier")
 
-os.chdir("src")
-
 setup(
     name = "thug",
     version = "0.7.3",
     author = "Angelo Dell'Aera",
     author_email = "buffer@antifork.org",
     description = "Low-interaction honeyclient Thug",
-    long_description = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "README.rst")).read(),
+    long_description = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.rst")).read(),
     classifiers = [
         "Development Status :: 4 - Beta",
         "Environment :: Console",
@@ -40,17 +38,17 @@ setup(
     packages = find_packages(),
     scripts = ["thug.py", ],
     data_files = [
-        (configuration_path, ["Analysis/honeyagent/honeyagent.conf.sample",
-                              "Analysis/virustotal/virustotal.conf.default",
-                              "Logging/logging.conf.default",
-                              "Plugins/plugins.conf.default"]),
-        (personalities_path, glob.glob("DOM/personalities/*.json")),
-        (rules_path        , glob.glob("Classifier/rules/*.yar")),
-        (js_rules_path     , glob.glob("Classifier/rules/jsclassifier/*.yar")),
-        (url_rules_path    , glob.glob("Classifier/rules/urlclassifier/*.yar")),
-        (sample_rules_path , glob.glob("Classifier/rules/sampleclassifier/*.yar")),
+        (configuration_path, ["src/nalysis/honeyagent/honeyagent.conf.sample",
+                              "src/Analysis/virustotal/virustotal.conf.default",
+                              "src/Logging/logging.conf.default",
+                              "src/Plugins/plugins.conf.default"]),
+        (personalities_path, glob.glob("src/DOM/personalities/*.json")),
+        (rules_path        , glob.glob("src/Classifier/rules/*.yar")),
+        (js_rules_path     , glob.glob("src/Classifier/rules/jsclassifier/*.yar")),
+        (url_rules_path    , glob.glob("src/Classifier/rules/urlclassifier/*.yar")),
+        (sample_rules_path , glob.glob("src/Classifier/rules/sampleclassifier/*.yar")),
     ],
-    install_requires = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "requirements.txt")).read().splitlines(),
+    install_requires = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")).read().splitlines(),
     entry_points = {
         "console_scripts": [
             "thug = thug:main",
