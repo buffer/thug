6ff10ae88c6235d239d45b8ad91783eb365f4c73
Angelo Dell'Aera <buffer@olografix.org>
2016-09-15 19:32:29 +0200

    Thug setup improvements

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ import os
 import glob
 from setuptools import setup, find_packages
 
+import thug
+
 configuration_path = "/etc/thug"
 
 personalities_path = os.path.join(configuration_path, "personalities") 
@@ -17,7 +19,7 @@ sample_rules_path  = os.path.join(rules_path, "sampleclassifier")
 
 setup(
     name = "thug",
-    version = "0.7.3",
+    version = thug.__version__,
     author = "Angelo Dell'Aera",
     author_email = "buffer@antifork.org",
     description = "Low-interaction honeyclient Thug",
@@ -32,26 +34,25 @@ setup(
         "Topic :: Security",
     ],
     package_data = {
-           "DOM"       : ["*.js"],
-           "Debugger"  : ["*.js"],
+           ""       : ["*.js"],
            },
     packages = find_packages(),
-    scripts = ["thug.py", ],
+    #scripts = ["thug/thug.py", ],
     data_files = [
-        (configuration_path, ["src/nalysis/honeyagent/honeyagent.conf.sample",
-                              "src/Analysis/virustotal/virustotal.conf.default",
-                              "src/Logging/logging.conf.default",
-                              "src/Plugins/plugins.conf.default"]),
-        (personalities_path, glob.glob("src/DOM/personalities/*.json")),
-        (rules_path        , glob.glob("src/Classifier/rules/*.yar")),
-        (js_rules_path     , glob.glob("src/Classifier/rules/jsclassifier/*.yar")),
-        (url_rules_path    , glob.glob("src/Classifier/rules/urlclassifier/*.yar")),
-        (sample_rules_path , glob.glob("src/Classifier/rules/sampleclassifier/*.yar")),
+        (configuration_path, ["thug/Analysis/honeyagent/honeyagent.conf.sample",
+                              "thug/Analysis/virustotal/virustotal.conf.default",
+                              "thug/Logging/logging.conf.default",
+                              "thug/Plugins/plugins.conf.default"]),
+        (personalities_path, glob.glob("thug/DOM/personalities/*.json")),
+        (rules_path        , glob.glob("thug/Classifier/rules/*.yar")),
+        (js_rules_path     , glob.glob("thug/Classifier/rules/jsclassifier/*.yar")),
+        (url_rules_path    , glob.glob("thug/Classifier/rules/urlclassifier/*.yar")),
+        (sample_rules_path , glob.glob("thug/Classifier/rules/sampleclassifier/*.yar")),
     ],
     install_requires = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")).read().splitlines(),
     entry_points = {
         "console_scripts": [
-            "thug = thug:main",
+            "thug = thug.thug:main",
         ]
     }
 )
