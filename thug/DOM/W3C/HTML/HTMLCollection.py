3cd9ebed144bd2413797e079854ff3dc2db5c6ac
Angelo Dell'Aera <buffer@olografix.org>
2016-09-24 15:12:35 +0200

    Code cleanup (patch 8)

--- thug/DOM/W3C/HTML/HTMLCollection.py
+++ thug/DOM/W3C/HTML/HTMLCollection.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
-
 from thug.DOM.JSClass import JSClass
 
 class HTMLCollection(JSClass):
