75e493e5eb4f99c629e737f9b528f31a3c612dd0
Angelo Dell'Aera <buffer@olografix.org>
2016-09-24 15:12:56 +0200

    Code cleanup (patch 9)

--- thug/DOM/W3C/HTML/HTMLElement.py
+++ thug/DOM/W3C/HTML/HTMLElement.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
-
 try:
     from io import StringIO
 except ImportError:
