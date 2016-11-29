91b9a3a6b1278ba2fef0198f22e7151359d09f4e
Angelo Dell'Aera <buffer@olografix.org>
2016-11-29 13:04:29 +0100

    Minor change

--- thug/ThugAPI/ThugAPI.py
+++ thug/ThugAPI/ThugAPI.py
@@ -287,7 +287,7 @@ class ThugAPI(object):
         content   = open(url, 'r').read()
         extension = os.path.splitext(url)
 
-        if len(extension) > 1 and extension[1].lower() in ('.js'):
+        if len(extension) > 1 and extension[1].lower() in ('.js', ):
             html = tostring(E.HTML(E.BODY(E.SCRIPT(content))))
         else:
             html = content
