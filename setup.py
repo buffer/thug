2b3fbc0777ae9300de570d95947752900ca95f72
Angelo Dell'Aera <buffer@olografix.org>
2016-12-01 15:20:30 +0100

    Minor fix

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ rules_path         = os.path.join(thug.__configuration_path__, "rules")
 scripts_path       = os.path.join(thug.__configuration_path__, "scripts")
 plugins_path       = os.path.join(thug.__configuration_path__, "plugins")
 hooks_path         = os.path.join(thug.__configuration_path__, "hooks")
+html_rules_path    = os.path.join(rules_path, "htmlclassifier")
 js_rules_path      = os.path.join(rules_path, "jsclassifier")
 url_rules_path     = os.path.join(rules_path, "urlclassifier")
 sample_rules_path  = os.path.join(rules_path, "sampleclassifier")
@@ -55,6 +56,7 @@ setup(
                                        "thug/Debugger/d8.js"]),
         (plugins_path               , []),
         (hooks_path                 , []),
+        (html_rules_path            , glob.glob("thug/Classifier/rules/htmlclassifier/*.yar")),
         (js_rules_path              , glob.glob("thug/Classifier/rules/jsclassifier/*.yar")),
         (url_rules_path             , glob.glob("thug/Classifier/rules/urlclassifier/*.yar")),
         (sample_rules_path          , glob.glob("thug/Classifier/rules/sampleclassifier/*.yar")),
