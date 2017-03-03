85ca865f96494915f571da55594238887c408252
Angelo Dell'Aera <buffer@olografix.org>
2017-03-03 11:53:16 +0100

    M minor improvements (patch 2)

--- thug/DOM/ClipboardData.py
+++ thug/DOM/ClipboardData.py
@@ -16,8 +16,10 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
+from .JSClass import JSClass
 
-class ClipboardData(object):
+
+class ClipboardData(JSClass):
     def __init__(self):
         self._data = dict()
 
