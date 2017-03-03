124e0712e24f925f58cd8456fab5e8df83a9aee3
Angelo Dell'Aera <buffer@olografix.org>
2017-03-03 11:55:06 +0100

    DOM minor improvements (patch 6)

--- thug/DOM/Opera.py
+++ thug/DOM/Opera.py
@@ -16,7 +16,9 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
+from .JSClass import JSClass
 
-class Opera(object):
+
+class Opera(JSClass):
     def __init__(self):
         pass
