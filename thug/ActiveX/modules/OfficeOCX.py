abf359144863e624e0568e962515a816e4946288
Angelo Dell'Aera <buffer@olografix.org>
2017-03-29 15:36:43 +0200

    Logging improvements (patch 22)

--- thug/ActiveX/modules/OfficeOCX.py
+++ thug/ActiveX/modules/OfficeOCX.py
@@ -12,12 +12,15 @@ def OpenWebFile(self, _file):
     log.ThugLogging.add_behavior_warn("[Office OCX ActiveX] Fetching from URL %s" % (_file, ))
     log.ThugLogging.log_exploit_event(self._window.url,
                                       "Office OCX ActiveX",
-                                      "OpenWebFile Arbitrary Program Execution Vulnerability",
+                                      "OpenWebFile Arbitrary Program Execution Vulnerability (BID-33243)",
                                       forward = False,
                                       data = {
                                                 "url" : _file
                                              }
                                      )
+
+    log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "BID-33243", None)
+
     try:
         self._window._navigator.fetch(_file, redirect_type = "Office OCX Exploit")
     except:  # pylint:disable=bare-except
