17152acc6e332952ae0ff2c81704f296a15eea01
Angelo Dell'Aera <buffer@olografix.org>
2017-03-29 15:40:28 +0200

    Logging improvements (patch 33)

--- thug/ActiveX/modules/VLC.py
+++ thug/ActiveX/modules/VLC.py
@@ -13,6 +13,8 @@ def getVariable(self, arg):
                                           "getVariable Overflow",
                                           cve = "CVE-2007-6262")
 
+        log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2007-6262", None)
+
 
 def setVariable(self, arg0, arg1):
     if len(arg0) > 255 or len(arg1) > 255:
@@ -21,6 +23,7 @@ def setVariable(self, arg0, arg1):
                                           "setVariable Overflow",
                                           cve = "CVE-2007-6262")
 
+        log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2007-6262", None)
 
 def addTarget(self, arg0, arg1, arg2, arg3):
     if len(arg0) > 255 or len(arg1) > 255 or len(arg2) > 255 or len(arg3) > 255:
@@ -28,3 +31,5 @@ def addTarget(self, arg0, arg1, arg2, arg3):
                                           "VLC ActiveX",
                                           "addTarget Overflow",
                                           cve = "CVE-2007-6262")
+
+        log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2007-6262", None)
