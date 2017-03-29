8da9ca03cafa32291124175c1635c1268c6df0e0
Angelo Dell'Aera <buffer@olografix.org>
2017-03-29 15:32:49 +0200

    Logging improvements (patch 12)

--- thug/ActiveX/modules/GomWeb.py
+++ thug/ActiveX/modules/GomWeb.py
@@ -12,3 +12,5 @@ def OpenURL(self, arg):
                                           "GOM Player Manager ActiveX",
                                           "Overflow in OpenURL",
                                           cve = "CVE-2007-5779")
+
+        log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2007-5779", None)
