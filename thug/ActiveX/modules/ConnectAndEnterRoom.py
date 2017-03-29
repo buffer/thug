7d508a03ac2ad98a9263092e896695d8db55271a
Angelo Dell'Aera <buffer@olografix.org>
2017-03-29 15:31:20 +0200

    Logging improvements (patch 8)

--- thug/ActiveX/modules/ConnectAndEnterRoom.py
+++ thug/ActiveX/modules/ConnectAndEnterRoom.py
@@ -12,3 +12,5 @@ def ConnectAndEnterRoom(self, arg0, arg1, arg2, arg3, arg4, arg5):
                                           "GlobalLink ConnectAndEnterRoom ActiveX",
                                           "ConnectAndEnterRoom Overflow",
                                           cve = 'CVE-2007-5722')
+
+        log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2007-5722", None)
