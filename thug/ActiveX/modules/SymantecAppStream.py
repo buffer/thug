4b82e8621490e829b0b0086a2bbfd97500b37c24
Angelo Dell'Aera <buffer@olografix.org>
2017-03-29 15:39:52 +0200

    Logging improvements (patch 31)

--- thug/ActiveX/modules/SymantecAppStream.py
+++ thug/ActiveX/modules/SymantecAppStream.py
@@ -10,15 +10,16 @@ def installAppMgr(self, url):
     log.ThugLogging.log_exploit_event(self._window.url,
                                       "Symantec AppStream LaunchObj ActiveX",
                                       "Arbitrary File Download and Execute",
-                                      cve = "CVE-2088-4388",
+                                      cve = "CVE-2008-4388",
                                       data = {
                                                 "url": url
                                              }
                                      )
 
+    log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2008-4388", None)
     log.ThugLogging.add_behavior_warn("[Symantec AppStream LaunchObj ActiveX] Fetching from URL %s" % (url, ))
 
     try:
-        self._window._navigator.fetch(url, redirect_type = "CVE-2088-4388")
+        self._window._navigator.fetch(url, redirect_type = "CVE-2008-4388")
     except:  # pylint:disable=bare-except
         log.ThugLogging.add_behavior_warn('[Symantec AppStream LaunchObj ActiveX] Fetch failed')
