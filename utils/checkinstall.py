7d336fd127a9fd87ebe6409e049d7e72dd1cc873
Angelo Dell'Aera <buffer@olografix.org>
2017-06-09 16:32:41 +0200

    Minor changes

--- utils/checkinstall.py
+++ utils/checkinstall.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python
 import sys
-print ("Checking proper Thug installation...")
+
+print("Checking proper Thug installation...")
+
 try:
     import bs4
-    import cffi
-    import chardet
+    import cchardet
     import cssutils
-    import decorator
-    import future
+    import elasticsearch
     import html5lib
-    import jsbeautifier
     import lxml
     import magic
     import networkx
     import pefile
-    import pika
-    import pycparser
     import pygraphviz
     import pylibemu
     import pymongo
-    import pyparsing
     import PyV8
     import rarfile
     import requests
@@ -32,15 +28,5 @@ except ImportError as error:
     print(error)
     print("Make sure to install above packages before running Thug.")
     sys.exit(-1)
-
-try:
-    from androguard.core import androconf
-    from androguard.core.bytecodes import apk
-    from androguard.core.bytecodes import dvm
-    from androguard.core.analysis import analysis
-except ImportError:
-    print("Androguard not found - APK analysis disabled")
-    print ("All requirements for Thug except Androguard are satisfied.")
-    sys.exit()
 
 print ("All requirements for Thug are satisfied.")
