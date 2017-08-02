3205d751279b0155e5d60787f634d08a072b8731
Angelo Dell'Aera <buffer@olografix.org>
2017-08-02 16:19:18 +0200

    Shellcode detection improvements (patch 50)

--- thug/ActiveX/modules/WinZip.py
+++ thug/ActiveX/modules/WinZip.py
@@ -14,3 +14,4 @@ def CreateNewFolderFromName(self, arg):
                                           cve = 'CVE-2006-6884')
 
         log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2006-6884", None)
+        log.DFT.check_shellcode(arg)
