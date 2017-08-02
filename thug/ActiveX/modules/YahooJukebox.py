e4a6fe0fa9d4dd397844caa58b6b143fa8d6f423
Angelo Dell'Aera <buffer@olografix.org>
2017-08-02 16:20:04 +0200

    Shellcode detection improvements (patch 52)

--- thug/ActiveX/modules/YahooJukebox.py
+++ thug/ActiveX/modules/YahooJukebox.py
@@ -11,6 +11,7 @@ def AddBitmap(self, arg0, arg1, arg2, arg3, arg4, arg5):
         log.ThugLogging.log_exploit_event(self._window.url,
                                           "Yahoo! Music Jukebox ActiveX",
                                           "Overflow in AddBitmap")
+        log.DFT.check_shellcode(arg1)
 
 
 def AddButton(self, arg0, arg1):
@@ -18,6 +19,7 @@ def AddButton(self, arg0, arg1):
         log.ThugLogging.log_exploit_event(self._window.url,
                                           "Yahoo! Music Jukebox ActiveX",
                                           "Overflow in AddButton")
+        log.DFT.check_shellcode(arg0)
 
 
 def AddImage(self, arg0, arg1):
@@ -25,3 +27,4 @@ def AddImage(self, arg0, arg1):
         log.ThugLogging.log_exploit_event(self._window.url,
                                           "Yahoo! Music Jukebox ActiveX",
                                           "Overflow in AddImage")
+        log.DFT.check_shellcode(arg0)
