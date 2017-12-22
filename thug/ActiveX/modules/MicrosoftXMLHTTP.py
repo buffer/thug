5974aedb7fb1b1f290c410c444dbb2409998851e
Angelo Dell'Aera <buffer@olografix.org>
2017-12-22 19:51:53 +0100

    [ActiveX] Microsoft.XMLHTTP readystatechange events emulation

--- thug/ActiveX/modules/MicrosoftXMLHTTP.py
+++ thug/ActiveX/modules/MicrosoftXMLHTTP.py
@@ -79,6 +79,8 @@ def send(self, varBody = None):
     if response is None:
         return 0
 
+    self.dispatchEvent("readystatechange")
+
     self.status          = response.status_code
     self.responseHeaders = response.headers
     self.responseBody    = response.text
