b6fb949bb29d3d47272a57656e60b519a27f4e1f
Angelo Dell'Aera <buffer@olografix.org>
2018-03-13 09:22:08 +0100

    [ActiveX] Shell.Application URLs extraction improvement

--- thug/ActiveX/modules/ShellApplication.py
+++ thug/ActiveX/modules/ShellApplication.py
@@ -1,23 +1,27 @@
 
+import re
 import logging
 
 log = logging.getLogger("Thug")
 
 
 def ShellExecute(self, sFile, vArguments = "", vDirectory = "", vOperation = "open", vShow = 1):
-    cmdLine = ''
+    cmdLine = "{} {}".format(sFile, vArguments)
 
     # Attempt to extract some URLs from the command line
-    urls = set()
+    # urls = set()
 
-    if 'http' in sFile:
-        for sep in ("'", '"'):
-            offset = cmdLine.find("{}http".format(sep))
-            if offset < 0:
-                continue
+    # if 'http' in cmdLine:
+    #    for sep in ("'", '"'):
+    #        offset = cmdLine.find("{}http".format(sep))
+    #        if offset < 0:
+    #            continue
+    #
+    #        url = cmdLine[offset + 1:].split("'")
+    #        urls.add(url[0])
 
-            url = cmdLine[offset + 1:].split("'")
-            urls.add(url[0])
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                      cmdLine.replace("'", '"'))
 
     log.ThugLogging.add_behavior_warn('[Shell.Application ActiveX] ShellExecute("{}", "{}", "{}", "{}", "{}")'.format(sFile,
                                                                                                                       vArguments,
