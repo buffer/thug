8e7a2cb5c58500e867557efda674ddf84597df16
Angelo Dell'Aera <buffer@olografix.org>
2018-04-11 11:07:35 -0700

    DOM W3C HTMLScriptElement improvements

--- thug/DOM/W3C/HTML/HTMLScriptElement.py
+++ thug/DOM/W3C/HTML/HTMLScriptElement.py
@@ -4,6 +4,10 @@ from .HTMLElement import HTMLElement
 from .attr_property import attr_property
 from .text_property import text_property
 
+import logging
+
+log = logging.getLogger("Thug")
+
 
 class HTMLScriptElement(HTMLElement):
     def __init__(self, doc, tag):
@@ -14,5 +18,14 @@ class HTMLScriptElement(HTMLElement):
     event           = None
     charset         = attr_property("charset", default = "")
     defer           = attr_property("defer", bool)
-    src             = attr_property("src", default = "")
+    _src            = attr_property("src", default = "")
     type            = attr_property("type")
+
+    def get_src(self):
+        return self._src
+
+    def set_src(self, src):
+        self._src = src
+        log.DFT.handle_script(self.tag)
+
+    src = property(get_src, set_src)
