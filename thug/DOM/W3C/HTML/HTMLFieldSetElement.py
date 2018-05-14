7200fd149b57bbd9b2b96bf0ad9f13c437df659e
Angelo Dell'Aera <buffer@olografix.org>
2018-05-14 16:18:42 +0200

    W3C DOM HTML HTMLFieldSetElement `disabled` attribute

--- thug/DOM/W3C/HTML/HTMLFieldSetElement.py
+++ thug/DOM/W3C/HTML/HTMLFieldSetElement.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 from .HTMLElement import HTMLElement
+from .attr_property import attr_property
 
 
 class HTMLFieldSetElement(HTMLElement):
     def __init__(self, doc, tag):
         HTMLElement.__init__(self, doc, tag)
+
+    disabled = attr_property("disabled", bool)
 
     @property
     def form(self):
