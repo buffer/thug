f6846ad898981dbdf4903dedb8e04fb8f1bc37d0
Angelo Dell'Aera <buffer@olografix.org>
2018-05-15 09:36:28 +0200

    W3C DOM HTML HTMLAnchorElement improvements

--- thug/DOM/W3C/HTML/HTMLAnchorElement.py
+++ thug/DOM/W3C/HTML/HTMLAnchorElement.py
@@ -3,6 +3,9 @@
 import logging
 import time
 import datetime
+
+from urlparse import urlparse
+
 from .HTMLElement import HTMLElement
 from .attr_property import attr_property
 from .compatibility import thug_long
@@ -14,18 +17,42 @@ class HTMLAnchorElement(HTMLElement):
     def __init__(self, doc, tag):
         HTMLElement.__init__(self, doc, tag)
 
-    accessKey       = attr_property("accesskey")
-    charset         = attr_property("charset", default = "")
-    coords          = attr_property("coords")
-    href            = attr_property("href")
-    hreflang        = attr_property("hreflang")
-    name            = attr_property("name")
-    rel             = attr_property("rel")
-    rev             = attr_property("rev")
-    shape           = attr_property("shape")
-    tabIndex        = attr_property("tabindex", thug_long)
-    target          = attr_property("target")
-    type            = attr_property("type")
+    accessKey = attr_property("accesskey")
+    charset   = attr_property("charset", default = "")
+    coords    = attr_property("coords")
+    href      = attr_property("href")
+    hreflang  = attr_property("hreflang")
+    name      = attr_property("name")
+    rel       = attr_property("rel")
+    rev       = attr_property("rev")
+    shape     = attr_property("shape")
+    tabIndex  = attr_property("tabindex", thug_long)
+    target    = attr_property("target")
+    type      = attr_property("type")
+
+    @property
+    def protocol(self):
+        if not self.href:
+            return ""
+
+        o = urlparse(self.href)
+        return ":{}".format(o.scheme) if o.scheme else ""
+
+    @property
+    def host(self):
+        o = urlparse(self.href)
+        return o.netloc if o.netloc else ""
+
+    @property
+    def hostname(self):
+        return self.host.split(":")[0]
+
+    @property
+    def port(self):
+        if ":" not in self.host:
+            return ""
+
+        return self.host.split(":")[1]
 
     def blur(self):
         pass
