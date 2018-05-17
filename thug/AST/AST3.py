169e762731e5f10321af1075d51581238a9368f7
Arushit Mudgal <arushitmudgal@gmail.com>
2018-05-17 18:46:48 +0530

    A minor typo

--- thug/AST/AST3.py
+++ thug/AST/AST3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# AST2.py
+# AST3.py
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
