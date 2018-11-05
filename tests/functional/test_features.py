a9cec874e1f113334b565a6f8e90fa5de3dd60dd
Angelo Dell'Aera <buffer@olografix.org>
2018-11-05 12:03:17 +0100

    [Functional tests] Features tests (patch 1)

---
+++ tests/functional/test_features.py
@@ -0,0 +1,53 @@
+import os
+import json
+import logging
+
+from thug.ThugAPI.ThugAPI import ThugAPI
+
+log = logging.getLogger("Thug")
+
+
+class TestFeatures(object):
+    thug_path = os.path.dirname(os.path.realpath(__file__)).split("thug")[0]
+    features_path = os.path.join(thug_path, "thug", "samples/features")
+    expected_path = os.path.join(thug_path, "thug/tests/functional/features.json")
+    
+    with open(expected_path) as fd:
+        expected = json.load(fd)
+
+    def do_perform_test(self, caplog, sample):
+        thug = ThugAPI()
+
+        thug.log_init(sample)
+
+        thug.set_useragent('win7ie90')
+        thug.set_verbose()
+        thug.set_json_logging()
+        thug.set_features_logging()
+
+        thug.log_init(sample)
+        thug.run_local(sample)
+        thug.log_event()
+
+        for r in caplog.records:
+            try:
+                features = json.dumps(r)
+            except Exception:
+                continue
+
+            if not isinstance(features, dict):
+                continue
+
+            if "html_count" not in features:
+                continue
+
+            for url in self.expected:
+                if not url.endswith(sample):
+                    continue
+
+                for key in features:
+                    assert features[key] == self.expected[url][key]
+
+    def test_test_1(self, caplog):
+        sample = os.path.join(self.features_path, "test1.html")
+        self.do_perform_test(caplog, sample)
