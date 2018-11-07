ef7bc2222fcade86e70463d41b29514bc721c900
Angelo Dell'Aera <buffer@olografix.org>
2018-11-07 09:16:06 +0100

    [Functional tests] Safari misc tests

---
+++ tests/functional/test_misc_safari.py
@@ -0,0 +1,212 @@
+import os
+import logging
+
+from thug.ThugAPI.ThugAPI import ThugAPI
+
+log = logging.getLogger("Thug")
+
+
+class TestMiscSamplesChrome(object):
+    thug_path = os.path.dirname(os.path.realpath(__file__)).split("thug")[0]
+    misc_path = os.path.join(thug_path, "thug", "samples/misc")
+
+    def do_perform_test(self, caplog, sample, expected):
+        thug = ThugAPI()
+
+        thug.set_useragent('osx10safari5')
+        thug.set_events('click,storage')
+        thug.disable_cert_logging()
+
+        thug.log_init(sample)
+        thug.run_local(sample)
+
+        records = [r.message for r in caplog.records]
+
+        matches = 0
+
+        for e in expected:
+            for record in records:
+                if e in record:
+                    matches += 1
+
+        assert matches >= len(expected)
+
+    def test_plugindetect1(self, caplog):
+        sample   = os.path.join(self.misc_path, "PluginDetect-0.7.6.html")
+        expected = ['AdobeReader version: 9.1.0.0',
+                    'Flash version: 10.0.64.0']
+
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_plugindetect2(self, caplog):
+        sample   = os.path.join(self.misc_path, "PluginDetect-0.7.8.html")
+        expected = ['AdobeReader version: 9,1,0,0',
+                    'Flash version: 10,0,64,0',
+                    'Java version: 1,6,0,32']
+
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_test1(self, caplog):
+        sample   = os.path.join(self.misc_path, "test1.html")
+        expected = ['[Window] Alert Text: one']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_test2(self, caplog):
+        sample   = os.path.join(self.misc_path, "test2.html")
+        expected = ['[Window] Alert Text: Java enabled: true']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_test3(self, caplog):
+        sample   = os.path.join(self.misc_path, "test3.html")
+        expected = ['[Window] Alert Text: foo']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testAppendChild(self, caplog):
+        sample   = os.path.join(self.misc_path, "testAppendChild.html")
+        expected = ["<div>Don't care about me</div>",
+                    '<div>Just a sample</div>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testCloneNode(self, caplog):
+        sample   = os.path.join(self.misc_path, "testCloneNode.html")
+        expected = ['<div id="cloned"><q>Can you copy <em>everything</em> I say?</q></div>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testCloneNode2(self, caplog):
+        sample   = os.path.join(self.misc_path, "testCloneNode2.html")
+        expected = ['<button align="left" id="myButton">Clone node</button>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testCreateHTMLDocument(self, caplog):
+        sample   = os.path.join(self.misc_path, "testCreateHTMLDocument.html")
+        expected = ['<html><head><title>New Document</title></head><body><p>This is a new paragraph.</p></body></html>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testDocumentAll(self, caplog):
+        sample   = os.path.join(self.misc_path, "testDocumentAll.html")
+        expected = ['<a href="http://www.google.com">Google</a>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testDocumentWrite1(self, caplog):
+        sample   = os.path.join(self.misc_path, "testDocumentWrite1.html")
+        expected = ['Foobar',
+                    "Google</a><script>alert('foobar');</script><script language=\"VBScript\">alert('Gnam');</script><script>alert('Aieeeeee');</script></body>"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testGetElementsByClassName(self, caplog):
+        sample   = os.path.join(self.misc_path, "testGetElementsByClassName.html")
+        expected = ['<div class="example">First</div>',
+                    '<div class="example">Hello World!</div>',
+                    '<div class="example">Second</div>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testInnerHTML(self, caplog):
+        sample   = os.path.join(self.misc_path, "testInnerHTML.html")
+        expected = ['dude', 'Fred Flinstone']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testInsertBefore(self, caplog):
+        sample   = os.path.join(self.misc_path, "testInsertBefore.html")
+        expected = ["<div>Just a sample</div><div>I'm your reference!</div></body></html>"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testLocalStorage(self, caplog):
+        sample   = os.path.join(self.misc_path, "testLocalStorage.html")
+        expected = ["Alert Text: Fired",
+                    "Alert Text: bar",
+                    "Alert Text: south"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testPlugins(self, caplog):
+        sample   = os.path.join(self.misc_path, "testPlugins.html")
+        expected = ["Shockwave Flash 10.0.64.0",
+                    "Windows Media Player 7",
+                    "Adobe Acrobat"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testNode(self, caplog):
+        sample   = os.path.join(self.misc_path, "testNode.html")
+        expected = ['<a href="/" id="thelink">test</a>',
+                    "thediv"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testNode2(self, caplog):
+        sample   = os.path.join(self.misc_path, "testNode2.html")
+        expected = ['<a href="/bar.html" id="thelink">test</a>',
+                    "thediv2"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testQuerySelector(self, caplog):
+        sample   = os.path.join(self.misc_path, "testQuerySelector.html")
+        expected = ["Alert Text: Have a Good life.",
+                    "CoursesWeb.net"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testQuerySelector2(self, caplog):
+        sample   = os.path.join(self.misc_path, "testQuerySelector2.html")
+        expected = ['<li class="aclass">CoursesWeb.net</li>',
+                    "<li>MarPlo.net</li>",
+                    '<li class="aclass">php.net</li>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testScope(self, caplog):
+        sample   = os.path.join(self.misc_path, "testScope.html")
+        expected = ["foobar",
+                    "foo",
+                    "bar",
+                    "True",
+                    "3",
+                    "2012-10-07 11:13:00",
+                    "3.14159265359",
+                    "/foo/i"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testSessionStorage(self, caplog):
+        sample   = os.path.join(self.misc_path, "testSessionStorage.html")
+        expected = ["key1",
+                    "key2",
+                    "value1",
+                    "value3"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testSetInterval(self, caplog):
+        sample   = os.path.join(self.misc_path, "testSetInterval.html")
+        expected = ["[Window] Alert Text: Hello"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testText(self, caplog):
+        sample   = os.path.join(self.misc_path, "testText.html")
+        expected = ['<p id="p1">First line of paragraph.<br/> Some text added dynamically. </p>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testWindowOnload(self, caplog):
+        sample   = os.path.join(self.misc_path, "testWindowOnload.html")
+        expected = ["[Window] Alert Text: Fired"]
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testInsertAdjacentHTML1(self, caplog):
+        sample   = os.path.join(self.misc_path, "testInsertAdjacentHTML1.html")
+        expected = ['<div id="five">five</div><div id="one">one</div>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testInsertAdjacentHTML2(self, caplog):
+        sample   = os.path.join(self.misc_path, "testInsertAdjacentHTML2.html")
+        expected = ['<div id="two"><div id="six">six</div>two</div>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testInsertAdjacentHTML3(self, caplog):
+        sample   = os.path.join(self.misc_path, "testInsertAdjacentHTML3.html")
+        expected = ['<div id="three">three<div id="seven">seven</div></div>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testInsertAdjacentHTML4(self, caplog):
+        sample   = os.path.join(self.misc_path, "testInsertAdjacentHTML4.html")
+        expected = ['<div id="four">four</div><div id="eight">eight</div>']
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_testCurrentScript(self, caplog):
+        sample   = os.path.join(self.misc_path, "testCurrentScript.html")
+        expected = ["[Window] Alert Text: This page has scripts",
+                    "[Window] Alert Text: text/javascript",
+                    "[Window] Alert Text: Just a useless script"]
+        self.do_perform_test(caplog, sample, expected)
