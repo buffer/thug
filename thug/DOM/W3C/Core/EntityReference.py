de03bc28f4cada56364821a7d9d6ed0f870373bc
Angelo Dell'Aera <buffer@olografix.org>
2019-02-04 16:26:26 +0100

    DOM W3C Core EntityReference improvements

--- thug/DOM/W3C/Core/EntityReference.py
+++ thug/DOM/W3C/Core/EntityReference.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+import bs4 as BeautifulSoup
+
 from .Node import Node
 
 
 class EntityReference(Node):
     def __init__(self, doc, name):
-        self.name = name
+        self.tag = BeautifulSoup.BeautifulSoup("&{};".format(name), "lxml")
         Node.__init__(self, doc)
+
+    @property
+    def name(self):
+        return self.tag.string.encode('utf8')
 
     @property
     def nodeName(self):
