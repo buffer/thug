b01152f1c35e3daf90e6eddf63d875464e470cde
Angelo Dell'Aera <buffer@olografix.org>
2019-03-06 15:55:22 +0100

    W3C DOM HTML HTMLFormElement improvements

--- thug/DOM/W3C/HTML/HTMLFormElement.py
+++ thug/DOM/W3C/HTML/HTMLFormElement.py
@@ -3,12 +3,21 @@
 import logging
 
 from .HTMLElement import HTMLElement
+from .HTMLFormControlsCollection import HTMLFormControlsCollection
 from .attr_property import attr_property
 
 log = logging.getLogger("Thug")
 
 
 class HTMLFormElement(HTMLElement):
+    name          = attr_property("name")
+    acceptCharset = attr_property("accept-charset", default = "")
+    action        = attr_property("action")
+    enctype       = attr_property("enctype", default = "application/x-www-form-urlencoded")
+    encoding      = attr_property("enctype", default = "application/x-www-form-urlencoded")
+    method        = attr_property("method", default = "get")
+    target        = attr_property("target", default = "")
+
     def __init__(self, doc, tag):
         HTMLElement.__init__(self, doc, tag)
 
@@ -25,18 +34,18 @@ class HTMLFormElement(HTMLElement):
 
     @property
     def elements(self):
-        raise NotImplementedError()
+        from thug.DOM.W3C.Core.DOMImplementation import DOMImplementation
+
+        nodes = []
+        for tag in self.tag.children:
+            if getattr(tag, 'name', None) and tag.name not in ('br', ):
+                nodes.append(DOMImplementation.createHTMLElement(self.doc, tag))
+
+        return HTMLFormControlsCollection(self.doc, nodes)
 
     @property
     def length(self):
-        raise NotImplementedError()
-
-    name            = attr_property("name")
-    acceptCharset   = attr_property("accept-charset", default = "UNKNOWN")
-    action          = attr_property("action")
-    enctype         = attr_property("enctype", default = "application/x-www-form-urlencoded")
-    method          = attr_property("method", default = "get")
-    target          = attr_property("target")
+        return len(self.elements)
 
     def submit(self):
         handler = getattr(log.DFT, 'do_handle_form', None)
