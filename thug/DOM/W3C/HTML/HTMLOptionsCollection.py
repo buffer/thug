b865ffb1768289c444df715556c5a20fdfc046aa
Angelo Dell'Aera <buffer@olografix.org>
2019-03-06 15:52:44 +0100

    Code cleanup

--- thug/DOM/W3C/HTML/HTMLOptionsCollection.py
+++ thug/DOM/W3C/HTML/HTMLOptionsCollection.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from thug.DOM.W3C.Core.DOMException import DOMException
 from .HTMLCollection import HTMLCollection
 
 
@@ -8,19 +7,3 @@ from .HTMLCollection import HTMLCollection
 class HTMLOptionsCollection(HTMLCollection):
     def __init__(self, doc, nodes):
         HTMLCollection.__init__(self, doc, nodes)
-
-    def getLength(self):
-        return len(self.nodes)
-
-    def setLength(self, value):
-        raise DOMException(DOMException.NOT_SUPPORTED_ERR)
-
-    length = property(getLength, setLength)
-
-    def namedItem(self, name):
-        for key in ('id', 'name', ):
-            for node in self.nodes:
-                if key in node.tag.attrs and node.tag.attrs[key] in (name, ):
-                    return node
-
-        return None
