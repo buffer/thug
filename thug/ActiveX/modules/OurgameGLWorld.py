88a3d1370e13585a8863cefd76798e7ab4e61130
Angelo Dell'Aera <buffer@olografix.org>
2019-03-22 17:11:02 +0100

    Minor changes

--- thug/ActiveX/modules/OurgameGLWorld.py
+++ thug/ActiveX/modules/OurgameGLWorld.py
@@ -21,7 +21,7 @@ def hgs_startNotify(self, arg):
     if len(arg) > 1000:
         log.ThugLogging.log_exploit_event(self._window.url,
                                           "Ourgame GLWorld ActiveX",
-                                          "Overflow in hgs_startGame",
+                                          "Overflow in hgs_startNotify",
                                           cve = 'CVE-2008-0647')
 
         log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2008-0647", None)
