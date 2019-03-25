64d04b976e145d8f1fd68514ccb015c71c673419
Angelo Dell'Aera <buffer@olografix.org>
2019-03-25 23:30:53 +0100

    [Unit tests] Add test_Views

---
+++ tests/DOM/test_Views.py
@@ -0,0 +1,7 @@
+from thug.DOM.W3C.Views.AbstractView import AbstractView
+
+class TestViews(object):
+    def testAbstractView(self):
+        view = AbstractView()
+        
+        assert view.document is None
