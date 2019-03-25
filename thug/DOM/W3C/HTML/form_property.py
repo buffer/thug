d2d2d5f04a3c9664bc3d9124e117cb9ac404bffd
Angelo Dell'Aera <buffer@olografix.org>
2019-03-25 15:57:48 +0100

    W3C DOM HTML form_property

---
+++ thug/DOM/W3C/HTML/form_property.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import six
+
+
+def form_property(default = None):
+    def getter(self):
+        from thug.DOM.W3C.Core.DOMImplementation import DOMImplementation
+
+        if not self.tag.parent.name.lower() in ('form', ):
+            return default
+
+        _form = getattr(self, '_form', None)
+        if _form is None:
+            self._form = DOMImplementation.createHTMLElement(self.doc, self.tag.parent)
+
+        return self._form
+
+    return property(getter)
