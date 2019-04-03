4a8670b28d7e7aa190a4d8c3beebbe969d353d5b
Angelo Dell'Aera <buffer@olografix.org>
2019-04-03 11:28:27 +0200

    [ActiveX] Microsoft.XMLDOM fixes and improvements

--- thug/ActiveX/modules/MicrosoftXMLDOM.py
+++ thug/ActiveX/modules/MicrosoftXMLDOM.py
@@ -4,20 +4,18 @@ import logging
 import base64
 import binascii
 
-from lxml import etree
-
 from thug.DOM.W3C import w3c
 from thug.OS.Windows import security_sys
-# from DOM.W3C.NamedNodeMap import NamedNodeMap
 
 log = logging.getLogger("Thug")
 
 
-class Node(etree.ElementBase):
-    def __init__(self, elementName):
+class Node(object):
+    def __init__(self, xml, elementName):
         self._nodeTypedValue = None
         self._dataType = None
-        etree.ElementBase.__init__(elementName)
+        self._xml = xml
+        self._node = xml.createElement(elementName)
 
     def getNodeTypedValue(self):
         try:
@@ -54,7 +52,6 @@ class Node(etree.ElementBase):
 
 def loadXML(self, bstrXML):
     self.xml = w3c.parseString(bstrXML)
-    # self.attributes = NamedNodeMap(self.xml._node)
 
     if "res://" not in bstrXML:
         return
@@ -67,15 +64,7 @@ def loadXML(self, bstrXML):
             if any(sys.lower() in p.lower() for sys in security_sys):
                 self.parseError._errorCode = 0
 
-    for p in bstrXML.split("'"):
-        if p.startswith("res://"):
-            log.URLClassifier.classify(p)
-            log.ThugLogging.add_behavior_warn("[Microsoft XMLDOM ActiveX] Attempting to load %s" % (p, ))
-            log.ThugLogging.log_classifier("exploit", log.ThugLogging.url, "CVE-2017-0022", None)
-            if any(sys.lower() in p.lower() for sys in security_sys):
-                self.parseError._errorCode = 0
-
 
 def createElement(self, bstrTagName):
     log.ThugLogging.add_behavior_warn("[Microsoft XMLDOM ActiveX] Creating element %s" % (bstrTagName, ))
-    return Node(bstrTagName)
+    return Node(self.xml, bstrTagName)
