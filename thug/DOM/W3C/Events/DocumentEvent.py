8dd3b403204d4a6a24b0b58f881950c4daa57e73
Angelo Dell'Aera <buffer@olografix.org>
2019-04-11 12:16:20 +0200

    W3C DOM Events DocumentEvent improvements

--- thug/DOM/W3C/Events/DocumentEvent.py
+++ thug/DOM/W3C/Events/DocumentEvent.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 from thug.DOM.W3C.Core.DOMException import DOMException
 
+from .HTMLEvent import HTMLEvent
+from .MouseEvent import MouseEvent
+from .MutationEvent import MutationEvent
+from .StorageEvent import StorageEvent
+from .UIEvent import UIEvent
+
+
+EventMap = {
+    "HTMLEvent"      : HTMLEvent,
+    "HTMLEvents"     : HTMLEvent,
+    "MouseEvent"     : MouseEvent,
+    "MouseEvents"    : MouseEvent,
+    "MutationEvent"  : MutationEvent,
+    "MutationEvents" : MutationEvent,
+    "StorageEvent"   : StorageEvent,
+    "UIEvent"        : UIEvent,
+    "UIEvents"       : UIEvent
+}
+
 
 # Introduced in DOM Level 2
 class DocumentEvent(object):
@@ -8,4 +27,7 @@ class DocumentEvent(object):
         self.doc = doc
 
     def createEvent(self, eventType):
-        raise DOMException(DOMException.NOT_SUPPORTED_ERR)
+        if eventType not in EventMap:
+            raise DOMException(DOMException.NOT_SUPPORTED_ERR)
+
+        return EventMap[eventType]()
