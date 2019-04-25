145847f2023b2086a75f0a00bd07fe061a8815dc
Angelo Dell'Aera <buffer@olografix.org>
2019-04-25 10:02:58 +0200

    Minor improvement

--- thug/ActiveX/modules/WinNTSystemInfo.py
+++ thug/ActiveX/modules/WinNTSystemInfo.py
@@ -6,37 +6,49 @@ import logging
 log = logging.getLogger("Thug")
 
 
-def ComputerName(self):
+@property
+def ComputerName():
     log.ThugLogging.add_behavior_warn("[WinNTSystemInfo ActiveX] Getting ComputerName")
+
     nlen = random.randint(6, 10)
     computerName = ''.join(
         random.choice(
             string.ascii_letters + string.digits) for _ in range(nlen))
+
     return computerName
 
 
-def DomainName(self):
+@property
+def DomainName():
     log.ThugLogging.add_behavior_warn("[WinNTSystemInfo ActiveX] Getting DomainName")
+
     nlen = random.randint(6, 10)
     domainName = ''.join(
         random.choice(
             string.ascii_letters + string.digits) for _ in range(nlen))
+
     return domainName
 
 
-def PDC(self):
+@property
+def PDC():
     log.ThugLogging.add_behavior_warn("[WinNTSystemInfo ActiveX] Getting PDC (Primary Domain Controller)")
+
     nlen = random.randint(6, 10)
     pdc = ''.join(
         random.choice(
             string.ascii_letters + string.digits) for _ in range(nlen))
+
     return pdc
 
 
-def UserName(self):
+@property
+def UserName():
     log.ThugLogging.add_behavior_warn("[WinNTSystemInfo ActiveX] Getting UserName")
+
     nlen = random.randint(6, 10)
     userName = ''.join(
         random.choice(
             string.ascii_letters + string.digits) for _ in range(nlen))
+
     return userName
