2056072e0dac9b949f2e4f3e8a001072125d0c4e
Angelo Dell'Aera <buffer@olografix.org>
2019-05-10 13:04:30 +0200

    [Unit tests] JS Filters

--- tests/Classifier/test_Classifiers.py
+++ tests/Classifier/test_Classifiers.py
@@ -19,12 +19,14 @@ class TestClassifiers:
 
         thug.add_htmlclassifier(os.path.join(self.signatures_path, "html_signature_1.yar"))
         thug.add_htmlfilter(os.path.join(self.signatures_path, "html_filter_2.yar"))
-        
+        thug.add_jsfilter(os.path.join(self.signatures_path, "js_signature_2.yar"))
+
         with open(os.path.join(self.samples_path, sample), 'r') as fd:
             html = fd.read()
 
         log.HTMLClassifier.classify(os.path.basename(sample), html)
         log.HTMLClassifier.filter(os.path.basename(sample), html)
+        log.JSClassifier.filter(os.path.basename(sample), html)
 
         records = [r.message for r in caplog.records]
 
@@ -46,5 +48,11 @@ class TestClassifiers:
     def test_html_filter2(self, caplog):
         sample   = os.path.join(self.samples_path, "test2.html")
         expected = ['[HTMLFILTER Classifier] URL: test2.html (Rule: html_filter_2, Classification: )']
+
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_js_filter2(self, caplog):
+        sample   = os.path.join(self.samples_path, "test2.html")
+        expected = ['[JSFILTER Classifier] URL: test2.html (Rule: js_signature_2, Classification: )']
 
         self.do_perform_test(caplog, sample, expected)
