8402e19f9d7bf01abc5109c63a09849e9a988a90
Angelo Dell'Aera <buffer@olografix.org>
2019-05-21 11:11:38 +0200

    [Unit tests] Plugin

---
+++ tests/DOM/test_Plugin.py
@@ -0,0 +1,16 @@
+from thug.DOM.Plugin import Plugin
+
+class TestPlugin(object):
+    def test_items(self):
+        plugin = Plugin()
+
+        plugin['test1'] = 'value1'
+        assert plugin['test1'] in ('value1', )
+
+        plugin.test2 = 'value2'
+        assert plugin.test2 in ('value2', )
+
+        del plugin['test1']
+        del plugin.test2
+
+        del plugin['test3']
