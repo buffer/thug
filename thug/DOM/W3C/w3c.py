96d529e98330ba8e46ef99657413bf92a356b9fa
Angelo Dell'Aera <buffer@olografix.org>
2019-05-24 09:50:13 +0200

    Code cleanup

--- thug/DOM/W3C/w3c.py
+++ thug/DOM/W3C/w3c.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 
 import six
-import bs4 as BeautifulSoup
+import bs4
 
 
 def getDOMImplementation(dom = None, **kwds):
     from thug.DOM.W3C.Core.DOMImplementation import DOMImplementation
-    return DOMImplementation(dom if dom else BeautifulSoup.BeautifulSoup('', 'lxml'), **kwds)
+    return DOMImplementation(dom if dom else bs4.BeautifulSoup('', 'lxml'), **kwds)
 
 
 def parseString(html, **kwds):
     from thug.DOM.W3C.Core.DOMImplementation import DOMImplementation
-    return DOMImplementation(BeautifulSoup.BeautifulSoup(html, "html.parser"), **kwds)
+    return DOMImplementation(bs4.BeautifulSoup(html, "html.parser"), **kwds)
 
 
-def parse(_file, **kwds):
-    if isinstance(_file, six.string_types):
-        with open(_file, 'r') as f:
-            return parseString(f.read())
-
-    return parseString(_file.read(), **kwds)
+# def parse(_file, **kwds):
+#    if isinstance(_file, six.string_types):
+#        with open(_file, 'r') as f:
+#            return parseString(f.read())
+#
+#    return parseString(_file.read(), **kwds)
