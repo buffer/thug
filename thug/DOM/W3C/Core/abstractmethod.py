486506309c08428a661cddf361ef8aa73a813b46
Angelo Dell'Aera <buffer@olografix.org>
2019-06-04 10:43:06 +0200

    Exclude code from coverage

--- thug/DOM/W3C/Core/abstractmethod.py
+++ thug/DOM/W3C/Core/abstractmethod.py
@@ -7,6 +7,6 @@ class abstractmethod(object):
     def __init__(self, func):
         self.func = func
 
-    def __call__(self, *args, **kwds):
+    def __call__(self, *args, **kwds): # pragma: no cover
         func_name = self.func.__name__ if sys.version_info.major >= 3 else self.func.func_name
         raise NotImplementedError("method %s is abstract." % func_name)
