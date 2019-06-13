b00bf31d9e2f29c5d4fab6f8ce4e8f3406c72d5f
Angelo Dell'Aera <buffer@olografix.org>
2019-06-13 13:01:43 +0200

    [Functional tests] ActiveX

--- tests/functional/test_disabled_activex.py
+++ tests/functional/test_disabled_activex.py
@@ -37,8 +37,14 @@ class TestDisabledActiveX(object):
         sample   = os.path.join(self.misc_path, "testObject6.html")
         expected = ['Unknown ActiveX Object: CA8A9780-280D-11CF-A24D-444553540000',
                     'Unknown ActiveX Object: 233C1507-6A77-46A4-9443-F871F945D258',
-                    'Unknown ActiveX object: agcontrol.agcontrol',
-                    'Unknown ActiveX object: javaplugin',
-                    'Unknown ActiveX object: JavaWebStart.isInstalled.1.7.0.0']
+                    'Unknown ActiveX Object: CAFEEFAC-DEC7-0000-0000-ABCDEFFEDCBA',
+                    'Unknown ActiveX object: 1234']
+
+        self.do_perform_test(caplog, sample, expected)
+
+    def test_disable_2(self, caplog):
+        sample   = os.path.join(self.misc_path, "PluginDetect-0.9.1.html")
+        expected = ['Unknown ActiveX Object: CAFEEFAC-DEC7-0000-0000-ABCDEFFEDCBA',
+                    'Unknown ActiveX Object: 5852F5ED-8BF4-11D4-A245-0080C6F74284']
 
         self.do_perform_test(caplog, sample, expected)
