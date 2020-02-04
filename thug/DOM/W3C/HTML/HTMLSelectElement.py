8dc772cdff2da3e3b14a9241a747880b95b7bf4d
Angelo Dell'Aera <buffer@olografix.org>
2020-02-04 11:23:52 +0100

    Code cleanup

--- thug/DOM/W3C/HTML/HTMLSelectElement.py
+++ thug/DOM/W3C/HTML/HTMLSelectElement.py
@@ -11,7 +11,7 @@ from .bool_property import bool_property
 class HTMLSelectElement(HTMLElement):
     selectedIndex = 0
     value         = None
-    disabled      = bool_property("disabled", readonly = False, novalue = False)
+    disabled      = bool_property("disabled")
     multiple      = bool_property("multiple")
     name          = attr_property("name")
     size          = attr_property("size", int)
--- thug/DOM/W3C/HTML/bool_property.py
+++ thug/DOM/W3C/HTML/bool_property.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 
-def bool_property(name, attrtype = bool, readonly = True, default = False, novalue = True):
+def bool_property(name, attrtype = bool, readonly = False, default = False):
     def getter(self):
-        if novalue:
-            return self.tag.has_attr(name)
-
         return attrtype(self.tag[name]) if self.tag.has_attr(name) else default
 
     def setter(self, value):
