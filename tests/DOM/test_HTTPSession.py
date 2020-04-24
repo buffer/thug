fa536f6f6cc948838fccb9d8a15f53f989a3f211
Angelo Dell'Aera <buffer@olografix.org>
2020-04-24 10:53:39 +0200

    [Unit tests] test_HTTPSession improvements

--- tests/DOM/test_HTTPSession.py
+++ tests/DOM/test_HTTPSession.py
@@ -16,6 +16,14 @@ log.personalities_path = os.path.join(configuration_path, "personalities") if co
 log.ThugOpts = ThugOpts()
 
 
+class WindowDict(dict):
+    def __setitem__(self, key, value):
+        self[key] = value
+
+    def __getitem__(self, key):
+        return self[key]
+
+
 class TestHTTPSession(object):
     def test_invalid_proxy_1(self):
         with pytest.raises(SystemExit):
@@ -31,3 +39,19 @@ class TestHTTPSession(object):
 
     def test_valid_proxy(self):
         s = HTTPSession(proxy = 'http://antifork.org:443')
+
+    def test_normalize_1(self):
+        window = WindowDict()
+        window.url = 'about:blank'
+
+        s = HTTPSession()
+        url = s._normalize_protocol_relative_url(window, '//www.google.com')
+        assert url == 'http://www.google.com'
+
+    def test_normalize_2(self):
+        window = WindowDict()
+        window.url = 'www.google.com'
+
+        s = HTTPSession()
+        url = s._normalize_protocol_relative_url(window, '//www.google.com')
+        assert url == 'http://www.google.com'
