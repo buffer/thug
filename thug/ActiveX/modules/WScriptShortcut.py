c504b47040787a8003629c3906181d4181f85e2b
Angelo Dell'Aera <buffer@olografix.org>
2020-06-26 09:01:28 +0200

    Code cleanup

--- thug/ActiveX/modules/WScriptShortcut.py
+++ thug/ActiveX/modules/WScriptShortcut.py
@@ -5,4 +5,3 @@ log = logging.getLogger("Thug")
 
 def save(self):
     log.ThugLogging.add_behavior_warn("[WScript.Shortcut ActiveX] Saving link object '{}' with target '{}'".format(self.FullName, self.TargetPath))
-    return
