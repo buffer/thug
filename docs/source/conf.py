cfeb9f2092c22eea759688482459debf82443664
Angelo Dell'Aera <buffer@olografix.org>
2021-01-04 10:19:28 +0100

    Update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Thug'
-copyright = u'2011-2020, Angelo Dell\'Aera'
+copyright = u'2011-2021, Angelo Dell\'Aera'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
