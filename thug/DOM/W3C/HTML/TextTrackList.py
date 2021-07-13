452418fd62f54f416a972fef9a012af32c939845
Angelo Dell'Aera <buffer@olografix.org>
2021-07-13 14:53:58 +0200

    [W3C] DOM HTML TextTrackList

---
+++ thug/DOM/W3C/HTML/TextTrackList.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from .HTMLCollection import HTMLCollection
+
+
+class TextTrackList(HTMLCollection):
+    def __init__(self, doc, tracks):
+        HTMLCollection.__init__(self, doc, tracks)
+
+    def getTrackById(self, id):
+        return None
