345e76670e795d03af4e224d3e99f1175473d1e5
Angelo Dell'Aera <buffer@olografix.org>
2021-07-13 18:41:57 +0200

    [W3C] DOM HTML TimeRanges

---
+++ thug/DOM/W3C/HTML/TimeRanges.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from thug.DOM.JSClass import JSClass
+from thug.DOM.W3C.Core.DOMException import DOMException
+
+
+class TimeRanges(JSClass):
+    def __init__(self, doc, ranges):
+        self.doc = doc
+        self.ranges = ranges
+
+    @property
+    def length(self):
+        return len(self.ranges)
+
+    def start(self, index):
+        if index in range(0, len(self.ranges)):
+            return self.ranges[0][0] # pragma: no cover
+        
+        raise DOMException(DOMException.INDEX_SIZE_ERR)
+
+    def end(self, index):
+        if index in range(0, len(self.ranges)):
+            return self.ranges[0][1] # pragma: no cover
+
+        raise DOMException(DOMException.INDEX_SIZE_ERR)
