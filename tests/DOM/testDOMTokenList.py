68db0fcbdb2f5d323fd1e66eaac4580c778781e1
Angelo Dell'Aera <buffer@olografix.org>
2021-07-19 11:33:08 +0200

    [W3C] Interface DOMTokenList

---
+++ tests/DOM/testDOMTokenList.py
@@ -0,0 +1,26 @@
+from thug.DOM.W3C.DOMTokenList import DOMTokenList
+
+class TestDOMTokenList(object):
+    def testDOMTokenList(self):
+        domtokenlist = DOMTokenList(['download', 'fullscreen', 'remoteplayback'])
+
+        assert domtokenlist.length == 0
+        assert domtokenlist.supports('download')
+
+        domtokenlist.add('foo')
+        assert domtokenlist.length == 0
+
+        assert domtokenlist.supports('nofullscreen')
+        domtokenlist.add('nofullscreen')
+        assert domtokenlist.length == 1
+
+        assert domtokenlist.item(0) == 'nofullscreen'
+        assert domtokenlist.contains('nofullscreen')
+
+        domtokenlist.replace('nofullscreen', 'fullscreen')
+        assert domtokenlist.contains('fullscreen')
+
+        domtokenlist.toggle('fullscreen')
+        domtokenlist.toggle('fullscreen')
+
+        assert domtokenlist.contains('fullscreen')
---
+++ thug/DOM/W3C/DOMTokenList.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import logging
+
+log = logging.getLogger("Thug")
+
+class DOMTokenList:
+    def __init__(self, supported, tokens = None):
+        self.tokens = list() if tokens is None else token
+        self.__init_supported(supported)
+
+    def __init_supported(self, supported):
+        self.supported = list()
+
+        for support in supported:
+            if support not in self.supported:
+                self.supported.append(support)
+
+            nosupport = "no{}".format(support)
+            if nosupport not in self.supported:
+                self.supported.append(nosupport)
+
+    @property
+    def length(self):
+        return len(self.tokens)
+
+    def item(self, index):
+        return self.tokens[index] if index in range(0, len(self.tokens)) else None
+
+    def contains(self, token):
+        return token in self.tokens
+
+    def add(self, token):
+        if token in self.supported and token not in self.tokens:
+            self.tokens.append(token)
+
+    def remove(self, token):
+        if token in self.tokens:
+            self.tokens.remove(token)
+
+    def toggle(self, token):
+        if token in self.tokens:
+            self.tokens.remove(token)
+            return
+
+        self.tokens.append(token)
+
+    def replace(self, oldToken, newToken):
+        self.remove(oldToken)
+        self.add(newToken)
+
+    def supports(self, token):
+        return token in self.supported
