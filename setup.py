75ca2fd7c3ff86fdd698e5bc5a005b966f1e8182
Angelo Dell'Aera <buffer@olografix.org>
2022-03-21 11:17:21 +0100

    Drop Python 3.7 support

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS :: MacOS X",
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
