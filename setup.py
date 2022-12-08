a275cfd7803312805ae1c6fbf381479067902ec5
Angelo Dell'Aera <buffer@olografix.org>
2022-12-08 11:35:59 +0100

    Move Thug setup configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -3,103 +3,34 @@
 import os
 import glob
 from setuptools import setup
-from setuptools import find_packages
 
 import thug
 
 os.environ['BUILD_LIB'] = '1'
 
-personalities_path = os.path.join(thug.__configuration_path__, "personalities")
-rules_path         = os.path.join(thug.__configuration_path__, "rules")
-scripts_path       = os.path.join(thug.__configuration_path__, "scripts")
-plugins_path       = os.path.join(thug.__configuration_path__, "plugins")
-hooks_path         = os.path.join(thug.__configuration_path__, "hooks")
-
-html_rules_path    = os.path.join(rules_path, "htmlclassifier")
-js_rules_path      = os.path.join(rules_path, "jsclassifier")
-vbs_rules_path     = os.path.join(rules_path, "vbsclassifier")
-url_rules_path     = os.path.join(rules_path, "urlclassifier")
-sample_rules_path  = os.path.join(rules_path, "sampleclassifier")
-text_rules_path    = os.path.join(rules_path, "textclassifier")
-cookie_rules_path  = os.path.join(rules_path, "cookieclassifier")
-image_rules_path   = os.path.join(rules_path, "imageclassifier")
-html_filter_path   = os.path.join(rules_path, "htmlfilter")
-js_filter_path     = os.path.join(rules_path, "jsfilter")
-vbs_filter_path    = os.path.join(rules_path, "vbsfilter")
-url_filter_path    = os.path.join(rules_path, "urlfilter")
-sample_filter_path = os.path.join(rules_path, "samplefilter")
-text_filter_path   = os.path.join(rules_path, "textfilter")
-cookie_filter_path = os.path.join(rules_path, "cookiefilter")
-image_filter_path  = os.path.join(rules_path, "imagefilter")
-
-
 setup(
-    name = "thug",
-    version = thug.__version__,
-    author = "Angelo Dell'Aera",
-    author_email = "angelo.dellaera@honeynet.org",
-    description = "Low-interaction honeyclient Thug",
-    license = "GPLv2",
-    long_description = open("README.rst", encoding = 'utf-8', mode = 'r').read(),
-    url = "http://buffer.github.io/thug/",
-    download_url = "https://github.com/buffer/thug/",
-    platforms = ["Linux", ],
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS :: MacOS X",
-        "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        "Topic :: Security",
-    ],
-    include_package_data = True,
-    package_data = {
-           ""       : ["*.js"],
-           },
-    packages = find_packages(),
-    data_files = [
-        (thug.__configuration_path__, ["conf/thug.conf", ]),
-        (thug.__configuration_path__, ["conf/inspector.json", ]),
-        (personalities_path         , glob.glob("conf/personalities/*.json")),
-        (rules_path                 , glob.glob("conf/rules/*.yar")),
-        (scripts_path               , ["thug/DOM/thug.js",
-                                       "thug/DOM/storage.js",
-                                       "thug/DOM/date.js",
-                                       "thug/DOM/eval.js",
-                                       "thug/DOM/write.js"]),
-        (plugins_path               , []),
-        (hooks_path                 , []),
-        (html_rules_path            , glob.glob("conf/rules/htmlclassifier/*.yar")),
-        (js_rules_path              , glob.glob("conf/rules/jsclassifier/*.yar")),
-        (vbs_rules_path             , glob.glob("conf/rules/vbsclassifier/*.yar")),
-        (url_rules_path             , glob.glob("conf/rules/urlclassifier/*.yar")),
-        (sample_rules_path          , glob.glob("conf/rules/sampleclassifier/*.yar")),
-        (text_rules_path            , glob.glob("conf/rules/textclassifier/*.yar")),
-        (cookie_rules_path          , glob.glob("conf/rules/cookieclassifier/*.yar")),
-        (image_rules_path           , glob.glob("conf/rules/imageclassifier/*.yar")),
-        (html_filter_path           , glob.glob("conf/rules/htmlfilter/*.yar")),
-        (js_filter_path             , glob.glob("conf/rules/jsfilter/*.yar")),
-        (vbs_filter_path            , glob.glob("conf/rules/vbsfilter/*.yar")),
-        (url_filter_path            , glob.glob("conf/rules/urlfilter/*.yar")),
-        (sample_filter_path         , glob.glob("conf/rules/samplefilter/*.yar")),
-        (text_filter_path           , glob.glob("conf/rules/textfilter/*.yar")),
-        (cookie_filter_path         , glob.glob("conf/rules/cookiefilter/*.yar")),
-        (image_filter_path          , glob.glob("conf/rules/imagefilter/*.yar")),
-    ],
-    install_requires = open("requirements.txt", encoding = 'utf-8', mode = 'r').read().splitlines(),
-    entry_points = {
-        "console_scripts": [
-            "thug = thug.thug:main",
-        ]
-    },
-    project_urls = {
-        'Bug Reports': 'https://github.com/buffer/thug/issues',
-        'Funding': 'https://buffer.github.io/thug/',
-        'Source': 'https://github.com/buffer/thug/',
-    },
+   data_files = [
+        (thug.__configuration_path__, ["conf/thug.conf", "conf/inspector.json"]),
+        (thug.__personalities_path__, glob.glob("conf/personalities/*.json")),
+        (thug.__rules_path__        , glob.glob("conf/rules/*.yar")),
+        (thug.__scripts_path__      , glob.glob("thug/DOM/scripts/*.js")),
+        (thug.__plugins_path__      , []),
+        (thug.__hooks_path__        , []),
+        (thug.__html_rules_path__   , glob.glob("conf/rules/htmlclassifier/*.yar")),
+        (thug.__js_rules_path__     , glob.glob("conf/rules/jsclassifier/*.yar")),
+        (thug.__vbs_rules_path__    , glob.glob("conf/rules/vbsclassifier/*.yar")),
+        (thug.__url_rules_path__    , glob.glob("conf/rules/urlclassifier/*.yar")),
+        (thug.__sample_rules_path__ , glob.glob("conf/rules/sampleclassifier/*.yar")),
+        (thug.__text_rules_path__   , glob.glob("conf/rules/textclassifier/*.yar")),
+        (thug.__cookie_rules_path__ , glob.glob("conf/rules/cookieclassifier/*.yar")),
+        (thug.__image_rules_path__  , glob.glob("conf/rules/imageclassifier/*.yar")),
+        (thug.__html_filter_path__  , glob.glob("conf/rules/htmlfilter/*.yar")),
+        (thug.__js_filter_path__    , glob.glob("conf/rules/jsfilter/*.yar")),
+        (thug.__vbs_filter_path__   , glob.glob("conf/rules/vbsfilter/*.yar")),
+        (thug.__url_filter_path__   , glob.glob("conf/rules/urlfilter/*.yar")),
+        (thug.__sample_filter_path__, glob.glob("conf/rules/samplefilter/*.yar")),
+        (thug.__text_filter_path__  , glob.glob("conf/rules/textfilter/*.yar")),
+        (thug.__cookie_filter_path__, glob.glob("conf/rules/cookiefilter/*.yar")),
+        (thug.__image_filter_path__ , glob.glob("conf/rules/imagefilter/*.yar")),
+   ],
 )
--- thug/__init__.py
+++ thug/__init__.py
@@ -1,7 +1,32 @@
+import os
+
 __version__            = "4.5"
 __jsengine__           = ""
 __jsengine_version__   = ""
 __configuration_path__ = "/etc/thug"
+
+__personalities_path__ = os.path.join(__configuration_path__, "personalities")
+__rules_path__         = os.path.join(__configuration_path__, "rules")
+__scripts_path__       = os.path.join(__configuration_path__, "scripts")
+__plugins_path__       = os.path.join(__configuration_path__, "plugins")
+__hooks_path__         = os.path.join(__configuration_path__, "hooks")
+
+__html_rules_path__    = os.path.join(__rules_path__, "htmlclassifier")
+__js_rules_path__      = os.path.join(__rules_path__, "jsclassifier")
+__vbs_rules_path__     = os.path.join(__rules_path__, "vbsclassifier")
+__url_rules_path__     = os.path.join(__rules_path__, "urlclassifier")
+__sample_rules_path__  = os.path.join(__rules_path__, "sampleclassifier")
+__text_rules_path__    = os.path.join(__rules_path__, "textclassifier")
+__cookie_rules_path__  = os.path.join(__rules_path__, "cookieclassifier")
+__image_rules_path__   = os.path.join(__rules_path__, "imageclassifier")
+__html_filter_path__   = os.path.join(__rules_path__, "htmlfilter")
+__js_filter_path__     = os.path.join(__rules_path__, "jsfilter")
+__vbs_filter_path__    = os.path.join(__rules_path__, "vbsfilter")
+__url_filter_path__    = os.path.join(__rules_path__, "urlfilter")
+__sample_filter_path__ = os.path.join(__rules_path__, "samplefilter")
+__text_filter_path__   = os.path.join(__rules_path__, "textfilter")
+__cookie_filter_path__ = os.path.join(__rules_path__, "cookiefilter")
+__image_filter_path__  = os.path.join(__rules_path__, "imagefilter")
 
 
 try:
