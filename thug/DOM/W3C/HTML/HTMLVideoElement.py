4b514507583a9d6f7160ff08106f7ee115fc7c61
Angelo Dell'Aera <buffer@olografix.org>
2023-08-31 16:36:17 +0200

    Fix typo

--- thug/DOM/W3C/HTML/HTMLVideoElement.py
+++ thug/DOM/W3C/HTML/HTMLVideoElement.py
@@ -11,8 +11,8 @@ log = logging.getLogger("Thug")
 class HTMLVideoElement(HTMLMediaElement):
     width       = attr_property("width", int, default = 0)
     height      = attr_property("height", int, default = 0)
-    videoWidth  = attr_property("width", int, default = 0)
-    videoHeight = attr_property("height", int, default = 0)
+    videoWidth  = attr_property("videoWidth", int, default = 0)
+    videoHeight = attr_property("videoHeight", int, default = 0)
     playsInline = attr_property("playsInline", bool, default = False)
     poster      = attr_property("poster", default = "")
 
