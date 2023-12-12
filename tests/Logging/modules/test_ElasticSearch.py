c9fc7fe5b424d82820aceb40611ce36db371eb15
Angelo Dell'Aera <buffer@olografix.org>
2023-12-12 10:26:33 +0100

    Unit tests minor improvement

--- tests/Logging/modules/test_ElasticSearch.py
+++ tests/Logging/modules/test_ElasticSearch.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import configparser
+import pytest
 
 from mock import patch
 
@@ -26,8 +27,11 @@ config = configparser.ConfigParser()
 conf_file = os.path.join(log.configuration_path, 'thug.conf')
 config.read(conf_file)
 
+NOT_IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS", None) is None
+
 
 class TestElasticSearch:
+    @pytest.mark.skipif(NOT_IN_GITHUB_ACTIONS, reason="Test works just in Github Actions")
     def test_export(self):
         log.ThugOpts.elasticsearch_logging = True
         log.configuration_path = configuration_path
