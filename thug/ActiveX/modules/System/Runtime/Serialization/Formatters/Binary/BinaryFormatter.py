8abdc5e2b927d498b87440fd7e0f677782b5bc05
Angelo Dell'Aera <buffer@olografix.org>
2024-03-12 08:20:06 +0100

    Minor linting change

--- thug/ActiveX/modules/System/Runtime/Serialization/Formatters/Binary/BinaryFormatter.py
+++ thug/ActiveX/modules/System/Runtime/Serialization/Formatters/Binary/BinaryFormatter.py
@@ -1,8 +1,8 @@
 import logging
 
-log = logging.getLogger("Thug")
-
 from thug.ActiveX.modules.System.Runtime.Delegate import Delegate
+
+log = logging.getLogger("Thug")
 
 
 def Deserialize_2(self, buf):
