e0f9e4e5b498d97e7a39e305c9d6667aa09d5997
Angelo Dell'Aera <buffer@olografix.org>
2025-02-19 15:27:34 +0100

    Version 6.12

--- doc/static/documentation_options.js
+++ doc/static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '6.8',
+    VERSION: '6.12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ doc/static/js/main.js
@@ -0,0 +1,5 @@
+/*
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello Bulma!');
+});
+*/
